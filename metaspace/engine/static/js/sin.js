5936bb3181fe50c2fba6763473f57dd4579f7606
Sergey Nikolenko <snikolenko@gmail.com>
2015-04-26 20:24:42 +0300

    minor

--- metaspace/engine/static/js/sin.js
+++ metaspace/engine/static/js/sin.js
@@ -29,8 +29,8 @@ function sin_render_job(id, type, name) {
 
 function sin_render_substance_small(id, name) {
   var res = '<a href="/substance/' + id +'">' + name + '</a>';
-  if (name != null && name.length > 50) {
-    return res.substring(0, 50) + '...';
+  if (name != null && name.length > 40) {
+    return res.substring(0, 37) + '...';
   }
   return res;
 }
