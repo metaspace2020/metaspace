2b6b0b66397ded3c3c9a36947ea2a98f303dcb46
Sergey Nikolenko <snikolenko@gmail.com>
2015-04-26 16:30:07 +0300

    double the peaks

--- metaspace/engine/addpeaks.py
+++ metaspace/engine/addpeaks.py
@@ -18,10 +18,10 @@ for x in rows:
 	mzpeaks[x[0]] = get_lists_of_mzs(x[1])["grad_mzs"]
 
 with open("mzpeaks.csv", "w") as outfile:
-	outfile.write("\n".join(["%s;{%s}" % (k, ",".join(["%.4f" % x for x in mzpeaks[k]]) ) for k in mzpeaks if len(mzpeaks[k]) > 0 ]))
+	outfile.write("\n".join(["%s;{%s}" % (k, ",".join(["%.4f" % (2*x) for x in mzpeaks[k]]) ) for k in mzpeaks if len(mzpeaks[k]) > 0 ]))
 
 cur.execute("INSERT INTO job_result_data VALUES %s" %
-				",".join(['(%d, %d, %d, %.6f)' % (self.job_id, -1, k, v) for k,v in res_dict.iteritems()])
+				",".join(['(%d, %d, %d, %.6f)' % (self.job_id, -1, k, 2*v) for k,v in res_dict.iteritems()])
 			)
 
 
