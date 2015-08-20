47c53f964e9df008c5755209597fbc55594d2c5b
Dominik Fay <dominik.fay@gmx.de>
2015-08-20 15:23:26 +0200

    Assure correct order of calls
    Wait for color bar until total image is loaded because the min and max
    intensities required for the color bar are calculated during image
    generation

--- metaspace/engine/handlers.py
+++ metaspace/engine/handlers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*
 """
 .. module:: handlers
-    :synopsis: Handlers for the webserver.
+	:synopsis: Handlers for the webserver.
 
 .. moduleauthor:: Sergey Nikolenko <snikolenko@gmail.com>
 """
@@ -29,6 +29,7 @@ from engine.isocalc import get_iso_mzs
 # global variable for special case html files
 html_pages = {
 }
+from engine.isocalc import get_lists_of_mzs
 
 
 @gen.coroutine
@@ -245,132 +246,164 @@ class SimpleHtmlHandler(tornado.web.RequestHandler):
 
 
 class NewPngHandler(tornado.web.RequestHandler):
-    """A RequestHandler for producing pngs. Returns a single ion image for given dataset, formula, adduct and peak.
-    Caches the res_dict until a request arrives that requires computing a different res_dict.
+	'''A RequestHandler for producing pngs. Returns a single ion image for given dataset, formula, adduct and peak. Available at url /demo-png. Caches the res_dict until a request arrives that requires computing a different res_dict.
+	
+	The min and max intensity is also cached for the colorbar. It is required that the client download the total ion image before requesting the colorbar, since the min and max values are computed during the image generation.'''
+	cache = {}
+	minmax_cache = {}
 
-    The min and max intensity is also cached for the colorbar. It is required that the client download the total ion
-    image before requesting the colorbar, since the min and max values are computed during the image generation."""
-    cache = {}
-    minmax_cache = {}
+	@property
+	def db(self):
+		return self.application.db
+	
+	@gen.coroutine
+	def get(self, dataset_id, job_id, sf_id, sf, adduct=None, peak_id=None):
+		request_as_tuple = (dataset_id, job_id, sf_id, sf)
+		request_as_tuple_long = (dataset_id, job_id, sf_id, sf, adduct, peak_id)
+		if self.request.uri.split('/')[1] == "mzimage_meta":
+			min_val, max_val = NewPngHandler.minmax_cache[request_as_tuple_long]
+			self.write(json.dumps({"min":min_val, "max":max_val}))
+			return
+		colormap = ((0x35, 0x2A, 0x87), (0x02, 0x68, 0xE1), (0x10, 0x8E, 0xD2), (0x0F, 0xAE, 0xB9), (0x65, 0xBE, 0x86), (0xC0, 0xBC, 0x60), (0xFF, 0xC3, 0x37), (0xF9, 0xFB, 0x0E))
+		bitdepth = 8
+		query_id = "demosubst"
+		# cast args to int
+		peak_id, job_id, sf_id, dataset_id = int(get_id_from_slug(peak_id)) if peak_id else None, int(job_id), int(sf_id), int(dataset_id)
+		def flushed_callback(t0):
+			def callback():
+				my_print("Finished write in NewPngHandler. Took %s" % (datetime.now() - t0))
+			return callback
+		def res_dict():
+			# return immediately if result is cached.
+			if request_as_tuple in NewPngHandler.cache:
+				my_print("request_as_tuple found in cache, returning immediately.")
+				return NewPngHandler.cache[request_as_tuple]
+			else:
+				my_print("request was not cached; clearing cache")
+				NewPngHandler.cache.clear()
+			# coords_q = self.database.query( sql_queries['mzimage2coords'] % int(dataset_id) )
+			coords_q = self.db.query( sql_queries['democoords'] % dataset_id )
+			# coords = { row["index"] : [row["column"], row["row"]] for row in coords_q }
+			coords = { row["index"] : [row["x"], row["y"]] for row in coords_q }
+			dimensions = self.db.query("SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % (job_id))[0]
+			(nRows, nColumns) = ( int(dimensions["nrows"]), int(dimensions["ncols"]) )
+			final_query = sql_queries[query_id] % ( job_id, sf_id, sf_id )
+			res_list = self.db.query(final_query)
+			adduct_dict = {}
+			for row in res_list:
+				if adducts[ row["adduct"] ] not in adduct_dict:
+					adduct_dict[ adducts[ row["adduct"] ] ] = []
+				adduct_dict[ adducts[ row["adduct"] ] ].append(row)
+			res_dict = {
+				"data" : {k : sorted(v, key=lambda x: x["peak"]) for k,v in adduct_dict.iteritems()},
+				"coords" : coords,
+				"dimensions" : (nRows, nColumns)
+			}
+			for k, v in res_dict["data"].iteritems():
+				for imInd in xrange(len(v)):
+					v[imInd]["val"] = np.array(v[imInd]["val"])
+					im_q = np.percentile(v[imInd]["val"], 99.0)
+					im_rep =  v[imInd]["val"] > im_q
+					v[imInd]["val"][im_rep] = im_q
+					v[imInd]["val"] = [round(x, 2) for x in list(v[imInd]["val"])]
+			NewPngHandler.cache[request_as_tuple] = res_dict
+			my_print("stored res_dict in cache")
+			return res_dict
+		def image_data(res_dict):
+			if not adduct and not peak_id:
+				# total image
+				# flat objects out into a list
+				data_list = reduce(operator.add, res_dict["data"].values())
+				# write them to a dict
+				data_dict = defaultdict(float)
+				for data_obj in data_list:
+					for idx, val in zip(data_obj["sp"], data_obj["val"]):
+						data_dict[idx] += val
+				# convert it into the desired format
+				data = {
+					'sp' : data_dict.keys(),
+					'val' : data_dict.values()
+				}
+			else:
+				data = res_dict["data"][adduct][peak_id]
+			coords = res_dict["coords"]
+			nRows, nColumns = res_dict["dimensions"]
+			# find highest and lowest intensity
+			non_zero_intensities = filter(lambda x: x > 0, data["val"])
+			min_val = min(non_zero_intensities)
+			max_val = max(non_zero_intensities)
+			NewPngHandler.minmax_cache[request_as_tuple_long] = (min_val, max_val)
+			normalized_max_val = max_val- min_val
+			# normalize to byte (bitdepth=8)
+			im_new = [list(colormap[0])*nColumns for _ in range(nRows)]
+			for idx, val in zip(data["sp"], data["val"]):
+				x,y = coords[idx]
+				if val == 0:
+					new_val = 0
+				else:
+					new_val = int(255 * (val - min_val)/normalized_max_val)
+				chunk_size = math.ceil(2.0**bitdepth / (len(colormap)-1))
+				color_chunk = int(new_val//chunk_size)
+				pos_in_chunk = new_val % chunk_size
+				l_chunk, u_chunk = colormap[color_chunk:color_chunk+2]
+				colors = list(colormap[0])
+				for i,(l,u) in enumerate(zip(l_chunk, u_chunk)):
+					colors[i] = int(l + (u-l)*pos_in_chunk/float(chunk_size))
+				im_new[y][3*x:3*x+3] = colors
+			return im_new, (nColumns, nRows)
+		def wrapper(res, res_list):
+			res_list.append(res)
+			
+		if args.time_profiling_enabled:
+			res = []
+			cProfile.runctx("wrapper(image_data(res_dict()), res)", globals(), locals())
+			im_data, size = res[0]
+		else:
+			im_data, size = image_data(res_dict())
+		fp = cStringIO.StringIO()
+		write_image(im_data, fp, size=size)
+		self.set_header("Content-Type", "image/png")
+		self.write(fp.getvalue())
 
-    @property
-    def db(self):
-        return self.application.db
 
-    @gen.coroutine
-    def get(self, dataset_id, job_id, sf_id, sf, adduct=None, peak_id=None):
-        request_as_tuple = (dataset_id, job_id, sf_id, sf)
-        request_as_tuple_long = (dataset_id, job_id, sf_id, sf, adduct, peak_id)
-        if self.request.uri.split('/')[1] == "mzimage_meta":
-            min_val, max_val = NewPngHandler.minmax_cache[request_as_tuple_long]
-            self.write(json.dumps({"min": min_val, "max": max_val}))
-            return
-        colormap = ((0x35, 0x2A, 0x87), (0x02, 0x68, 0xE1), (0x10, 0x8E, 0xD2), (0x0F, 0xAE, 0xB9), (0x65, 0xBE, 0x86),
-                    (0xC0, 0xBC, 0x60), (0xFF, 0xC3, 0x37), (0xF9, 0xFB, 0x0E))
-        bitdepth = 8
-        query_id = "demosubst"
-        # cast args to int
-        peak_id, job_id, sf_id, dataset_id = int(get_id_from_slug(peak_id)) if peak_id else None, int(job_id), int(
-            sf_id), int(dataset_id)
+class MZImageHandler(tornado.web.RequestHandler):
+	'''Tornado handler for a png m/z image (not used in current version).'''
+	@property
+	def db(self):
+		return self.application.db
 
-        def flushed_callback(t0):
-            def callback():
-                my_print("Finished write in NewPngHandler. Took %s" % (datetime.now() - t0))
+	@gen.coroutine
+	def get(self, job_string):
+		my_print(job_string)
+		job_id, peak_id = get_formula_and_peak(job_string)
+		my_print("Creating m/z image for job %d..." % job_id)
+		params = self.db.query("SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % (job_id))[0]
+		(dRows, dColumns) = ( int(params["nrows"]), int(params["ncols"]) )
+		if peak_id > -1:
+			data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d AND peak=%d" % (job_id, peak_id))
+		else:
+			data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d" % job_id)
+		sio = write_image( make_image_arrays(dRows, dColumns, [int(row["s"]) for row in data], [float(row["v"]) for row in data]) )
+		self.set_header("Content-Type", "image/png")
+		self.write(sio.getvalue())
 
-            return callback
 
-        def res_dict():
-            # return immediately if result is cached.
-            if request_as_tuple in NewPngHandler.cache:
-                my_print("request_as_tuple found in cache, returning immediately.")
-                return NewPngHandler.cache[request_as_tuple]
-            else:
-                my_print("request was not cached; clearing cache")
-                NewPngHandler.cache.clear()
-            # coords_q = self.database.query( sql_queries['mzimage2coords'] % int(dataset_id) )
-            coords_q = self.db.query(sql_queries['democoords'] % dataset_id)
-            # coords = { row["index"] : [row["column"], row["row"]] for row in coords_q }
-            coords = {row["index"]: [row["x"], row["y"]] for row in coords_q}
-            dimensions = self.db.query(
-                "SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % job_id)[
-                0]
-            (nRows, nColumns) = (int(dimensions["nrows"]), int(dimensions["ncols"]))
-            final_query = sql_queries[query_id] % (job_id, sf_id, sf_id)
-            res_list = self.db.query(final_query)
-            adduct_dict = {}
-            for row in res_list:
-                if adducts[row["adduct"]] not in adduct_dict:
-                    adduct_dict[adducts[row["adduct"]]] = []
-                adduct_dict[adducts[row["adduct"]]].append(row)
-            res_dict = {
-                "data": {k: sorted(v, key=lambda x: x["peak"]) for k, v in adduct_dict.iteritems()},
-                "coords": coords,
-                "dimensions": (nRows, nColumns)
-            }
-            for k, v in res_dict["data"].iteritems():
-                for imInd in xrange(len(v)):
-                    v[imInd]["val"] = np.array(v[imInd]["val"])
-                    im_q = np.percentile(v[imInd]["val"], 99.0)
-                    im_rep = v[imInd]["val"] > im_q
-                    v[imInd]["val"][im_rep] = im_q
-                    v[imInd]["val"] = [round(x, 2) for x in list(v[imInd]["val"])]
-            NewPngHandler.cache[request_as_tuple] = res_dict
-            my_print("stored res_dict in cache")
-            return res_dict
+class MZImageParamHandler(tornado.web.RequestHandler):
+	'''Tornado handler for a png m/z image from a dataset job with parameter (not used in current version).'''
+	@property
+	def db(self):
+		return self.application.db
 
-        def image_data(res_dict):
-            if not adduct and not peak_id:
-                # total image
-                # flat objects out into a list
-                data_list = reduce(operator.add, res_dict["data"].values())
-                # write them to a dict
-                data_dict = defaultdict(float)
-                for data_obj in data_list:
-                    for idx, val in zip(data_obj["sp"], data_obj["val"]):
-                        data_dict[idx] += val
-                # convert it into the desired format
-                data = {
-                    'sp': data_dict.keys(),
-                    'val': data_dict.values()
-                }
-            else:
-                data = res_dict["data"][adduct][peak_id]
-            coords = res_dict["coords"]
-            nRows, nColumns = res_dict["dimensions"]
-            # find highest and lowest intensity
-            non_zero_intensities = filter(lambda x: x > 0, data["val"])
-            min_val = min(non_zero_intensities)
-            max_val = max(non_zero_intensities)
-            NewPngHandler.minmax_cache[request_as_tuple_long] = (min_val, max_val)
-            normalized_max_val = max_val - min_val
-            # normalize to byte (bitdepth=8)
-            im_new = [list(colormap[0]) * nColumns for _ in range(nRows)]
-            for idx, val in zip(data["sp"], data["val"]):
-                x, y = coords[idx]
-                if val == 0:
-                    new_val = 0
-                else:
-                    new_val = int(255 * (val - min_val) / normalized_max_val)
-                chunk_size = math.ceil(2.0 ** bitdepth / (len(colormap) - 1))
-                color_chunk = int(new_val // chunk_size)
-                pos_in_chunk = new_val % chunk_size
-                l_chunk, u_chunk = colormap[color_chunk:color_chunk + 2]
-                colors = list(colormap[0])
-                for i, (l, u) in enumerate(zip(l_chunk, u_chunk)):
-                    colors[i] = int(l + (u - l) * pos_in_chunk / float(chunk_size))
-                im_new[y][3 * x:3 * x + 3] = colors
-            return im_new, (nColumns, nRows)
+	@gen.coroutine
+	def get(self, job_string, param_string):
+		my_print(job_string)
+		job_id = int( get_id_from_slug(job_string) )
+		formula_id, peak_id = get_formula_and_peak(param_string)
+		my_print("Creating m/z image for job %d..." % job_id)
+		params = self.db.query("SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % job_id)[0]
+		(dRows, dColumns) = ( int(params["nrows"]), int(params["ncols"]) )
+		data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d AND param=%d AND peak=%d" % (job_id, formula_id, peak_id))
+		sio = write_image( make_image_arrays(dRows, dColumns, [int(row["s"]) for row in data], [float(row["v"]) for row in data]) )
+		self.set_header("Content-Type", "image/png")
+		self.write(sio.getvalue())
 
-        def wrapper(res, res_list):
-            res_list.append(res)
-
-        if args.time_profiling_enabled:
-            res = []
-            cProfile.runctx("wrapper(image_data(res_dict()), res)", globals(), locals())
-            im_data, size = res[0]
-        else:
-            im_data, size = image_data(res_dict())
-        fp = cStringIO.StringIO()
-        write_image(im_data, fp, size=size)
-        self.set_header("Content-Type", "image/png")
-        self.write(fp.getvalue())
