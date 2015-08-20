f53328ad71df78c931e867795f115db50759e343
shefali23 <shefali.sharma@gmail.com>
2015-08-20 18:18:06 +0530

    updated Sphinx documentation

--- metaspace/engine/docs/conf.py
+++ metaspace/engine/docs/conf.py
@@ -21,15 +21,30 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../engine'))
+sys.path.insert(0, os.path.abspath('../engine/pyMS'))
+sys.path.insert(0, os.path.abspath('../engine/pyIMS'))
+sys.path.insert(0, os.path.abspath('../engine/scripts'))
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +62,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Spatial Metabolomics'
-copyright = u'2015, Shefali Sharma'
-author = u'Shefali Sharma'
+copyright = u'2015, EMBL'
+author = u'Theodore Alexandrov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -108,12 +123,18 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+#html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+#html_theme_options = {
+#    "rightsidebar": "true",
+#    "relbarbgcolor": "black"
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -282,3 +303,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'http://docs.python.org/': None}
---
+++ metaspace/engine/docs/scripts.rst
@@ -0,0 +1,37 @@
+Scripts
+=======
+
+
+run_process_dataset
+-------------------
+
+.. automodule:: run_process_dataset
+	:members:
+	:noindex:
+
+run_insert_to_db
+----------------
+
+.. automodule:: run_insert_to_db
+	:members:
+	:noindex:
+
+run_aws_dataset
+---------------
+
+:file:`run_aws_dataset.sh`
+	:Synopsis: A bash script to run dataset processing on AWS. Has a lot of hardcoded stuff, take a look inside.
+
+run_aws_instance
+----------------
+
+:file:`run_aws_instance.sh`
+	:Synopsis: A bash script to run dataset processing on AWS. Has a lot of hardcoded stuff, take a look inside.
+
+addpeaks
+--------
+
+.. automodule:: addpeaks
+	:members:
+	:noindex:
+
