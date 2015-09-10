e0a4eba960e6120264d2c877499801074b0289db
Dominik Fay <dominik.fay@gmx.de>
2015-09-10 16:40:43 +0200

    Fix missing names and pep8

--- metaspace/engine/handlers_deprecated.py
+++ metaspace/engine/handlers_deprecated.py
@@ -7,56 +7,67 @@
 .. moduleauthor:: Sergey Nikolenko <snikolenko@gmail.com>
 """
 
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+from tornado import gen
+
+from engine.imaging import write_image, make_image_arrays
+from engine.util import my_print, get_id_from_slug
+from webserver import get_formula_and_peak
+
+
 # global variable for special case html files
 html_pages = {
 }
 
-import tornado.ioloop
-import tornado.web
-import tornado.httpserver
-from tornado.concurrent import Future
-from tornado import gen
-from tornado.ioloop import IOLoop
-import tornpsql
-
 
 class MZImageHandler(tornado.web.RequestHandler):
-	'''Tornado handler for a png m/z image (not used in current version).'''
-	@property
-	def db(self):
-		return self.application.db
+    """Tornado handler for a png m/z image (not used in current version)."""
 
-	@gen.coroutine
-	def get(self, job_string):
-		my_print(job_string)
-		job_id, peak_id = get_formula_and_peak(job_string)
-		my_print("Creating m/z image for job %d..." % job_id)
-		params = self.db.query("SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % (job_id))[0]
-		(dRows, dColumns) = ( int(params["nrows"]), int(params["ncols"]) )
-		if peak_id > -1:
-			data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d AND peak=%d" % (job_id, peak_id))
-		else:
-			data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d" % job_id)
-		sio = write_image( make_image_arrays(dRows, dColumns, [int(row["s"]) for row in data], [float(row["v"]) for row in data]) )
-		self.set_header("Content-Type", "image/png")
-		self.write(sio.getvalue())
+    @property
+    def db(self):
+        return self.application.db
+
+    @gen.coroutine
+    def get(self, job_string):
+        my_print(job_string)
+        job_id, peak_id = get_formula_and_peak(job_string)
+        my_print("Creating m/z image for job %d..." % job_id)
+        params = self.db.query(
+            "SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % job_id)[0]
+        (dRows, dColumns) = (int(params["nrows"]), int(params["ncols"]))
+        if peak_id > -1:
+            data = self.db.query(
+                "SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d AND peak=%d" % (job_id, peak_id))
+        else:
+            data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d" % job_id)
+        sio = write_image(
+            make_image_arrays(dRows, dColumns, [int(row["s"]) for row in data], [float(row["v"]) for row in data]))
+        self.set_header("Content-Type", "image/png")
+        self.write(sio.getvalue())
 
 
 class MZImageParamHandler(tornado.web.RequestHandler):
-	'''Tornado handler for a png m/z image from a dataset job with parameter (not used in current version).'''
-	@property
-	def db(self):
-		return self.application.db
+    """Tornado handler for a png m/z image from a dataset job with parameter (not used in current version)."""
 
-	@gen.coroutine
-	def get(self, job_string, param_string):
-		my_print(job_string)
-		job_id = int( get_id_from_slug(job_string) )
-		formula_id, peak_id = get_formula_and_peak(param_string)
-		my_print("Creating m/z image for job %d..." % job_id)
-		params = self.db.query("SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % job_id)[0]
-		(dRows, dColumns) = ( int(params["nrows"]), int(params["ncols"]) )
-		data = self.db.query("SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d AND param=%d AND peak=%d" % (job_id, formula_id, peak_id))
-		sio = write_image( make_image_arrays(dRows, dColumns, [int(row["s"]) for row in data], [float(row["v"]) for row in data]) )
-		self.set_header("Content-Type", "image/png")
-		self.write(sio.getvalue())
+    @property
+    def db(self):
+        return self.application.db
+
+    @gen.coroutine
+    def get(self, job_string, param_string):
+        my_print(job_string)
+        job_id = int(get_id_from_slug(job_string))
+        formula_id, peak_id = get_formula_and_peak(param_string)
+        my_print("Creating m/z image for job %d..." % job_id)
+        params = self.db.query(
+            "SELECT nrows,ncols FROM jobs j JOIN datasets d on j.dataset_id=d.dataset_id WHERE j.id=%d" % job_id)[0]
+        (dRows, dColumns) = (int(params["nrows"]), int(params["ncols"]))
+        data = self.db.query(
+            "SELECT spectrum as s,value as v FROM job_result_data WHERE job_id=%d AND param=%d AND peak=%d" % (
+                job_id, formula_id, peak_id))
+        sio = write_image(
+            make_image_arrays(dRows, dColumns, [int(row["s"]) for row in data], [float(row["v"]) for row in data]))
+        self.set_header("Content-Type", "image/png")
+        self.write(sio.getvalue())
