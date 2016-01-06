bbb25c00b47e61d2f2a861a69bb77b62e0d3bdb3
Vitaly <vitaly.kovalev@embl.de>
2016-01-06 17:03:49 +0100

    webapp filters bug fix

--- metaspace/engine/webapp/static/js/sm/sm.js
+++ metaspace/engine/webapp/static/js/sm/sm.js
@@ -122,8 +122,8 @@ $(document).ready(function() {
     yadcf.init(tbl_demo, [
       {column_number : 0, filter_type: "select", filter_container_id: "fil-db", filter_reset_button_text: false, filter_default_label: 'Select...'},
       {column_number : 1, filter_type: "select", filter_container_id: "fil-ds", filter_reset_button_text: false, filter_default_label: 'Select...', filter_match_mode: "exact"},
-      {column_number : 2, filter_type: "text", filter_container_id: "fil-nm", filter_reset_button_text: false },
-      {column_number : 3, filter_type: "text", filter_container_id: "fil-sf", filter_reset_button_text: false},
+      {column_number : 2, filter_type: "text", filter_container_id: "fil-sf", filter_reset_button_text: false},
+      {column_number : 3, filter_type: "text", filter_container_id: "fil-nm", filter_reset_button_text: false},
       {column_number : 4, filter_type: "text", filter_container_id: "fil-id", filter_reset_button_text: false},
       {column_number : 5, filter_type: "lower_bound_number", filter_container_id: "fil-chaos",
         filter_reset_button_text: false, filter_default_label: ['&ge;']
