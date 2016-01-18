b13d4c8a415af760a44aa95dfcf7e04da1242253
= <=>
2016-01-18 14:45:11 +0100

    mac os x installation script

---
+++ metaspace/engine/macosx/add_dataset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export PATH=~/miniconda2/bin:$PATH
+source activate sm_distributed
+export SPARK_HOME=$HOME/spark-1.6.0-bin-hadoop2.6/
+export PYTHONPATH=.:$SPARK_HOME/python:$PYTHONPATH
+cp conf/sm_log.cfg.template conf/sm_log.cfg
+python scripts/run_molecule_search.py --config macosx/config.json.template "$@"
