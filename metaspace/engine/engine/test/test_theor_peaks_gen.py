cf16597a2fe2b9cd917e75d11eae0ad7121f82e3
Vitaly Kovalev <vitaly.kovalev@embl.de>
2016-02-09 15:56:34 +0100

    tests update

--- metaspace/engine/engine/test/test_theor_peaks_gen.py
+++ metaspace/engine/engine/test/test_theor_peaks_gen.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 from mock import mock, MagicMock
 from numpy.testing import assert_array_almost_equal
-
 from engine.theor_peaks_gen import TheorPeaksGenerator, IsocalcWrapper
 from engine.db import DB
 from pyMS.mass_spectrum import MassSpectrum
@@ -36,7 +35,7 @@ def test_isocalc_get_iso_peaks_correct_sf_adduct(isocalc_isodist, ds_config):
     assert_array_almost_equal(peak_dict['centr_mzs'], get_spectrum_side_effect('centroids')[0])
     assert_array_almost_equal(peak_dict['centr_ints'], get_spectrum_side_effect('centroids')[1])
 
-    assert_array_almost_equal(peak_dict['profile_mzs'], [95., 99., 100., 101., 105., 195.,  199.,  200.,  201.,  205.])
+    assert_array_almost_equal(peak_dict['profile_mzs'], [95., 99., 100., 101., 105., 195., 199., 200., 201., 205.])
     assert_array_almost_equal(peak_dict['profile_ints'], [50., 90., 100., 90., 50., 5., 9., 10., 9., 5.])
 
 
@@ -54,14 +53,13 @@ def test_isocalc_wrapper_get_iso_peaks_wrong_sf_adduct(isocalc_isodist, ds_confi
 
 @mock.patch('engine.theor_peaks_gen.complete_isodist')
 def test_isocalc_wrapper_get_iso_peaks_check_profiles_correct(isocalc_isodist, ds_config):
-
     def get_spectrum_side_effect(source):
         if source == 'centroids':
             return (np.array([100., 200.]),
                     np.array([100., 10.]))
         elif source == 'profile':
             return (np.hstack([np.linspace(50, 150, 10000), np.linspace(150, 250, 10000)]),
-                    np.hstack([np.ones(10000)*100, np.ones(10000)*10]))
+                    np.hstack([np.ones(10000) * 100, np.ones(10000) * 10]))
 
     mock_mass_sp = MagicMock(spec=MassSpectrum)
     mock_mass_sp.get_spectrum.side_effect = get_spectrum_side_effect
@@ -82,10 +80,10 @@ def test_isocalc_wrapper_get_iso_peaks_check_profiles_correct(isocalc_isodist, d
 
     # profile points not too far or close to the centroids
     assert 0.1 < abs(peaks_dict['centr_mzs'][0] - peaks_dict['profile_mzs'][0]) < 0.2
-    assert 0.1 < abs(peaks_dict['centr_mzs'][0] - peaks_dict['profile_mzs'][pts_per_centr-1]) < 0.2
+    assert 0.1 < abs(peaks_dict['centr_mzs'][0] - peaks_dict['profile_mzs'][pts_per_centr - 1]) < 0.2
 
     assert 0.1 < abs(peaks_dict['centr_mzs'][1] - peaks_dict['profile_mzs'][pts_per_centr]) < 0.2
-    assert 0.1 < abs(peaks_dict['centr_mzs'][1] - peaks_dict['profile_mzs'][2*pts_per_centr-1]) < 0.2
+    assert 0.1 < abs(peaks_dict['centr_mzs'][1] - peaks_dict['profile_mzs'][2 * pts_per_centr - 1]) < 0.2
 
 
 @mock.patch('engine.theor_peaks_gen.IsocalcWrapper.isotope_peaks')
@@ -96,9 +94,10 @@ def test_formatted_iso_peaks_correct_input(iso_peaks_mock, ds_config):
 
     isocalc_wrapper = IsocalcWrapper(ds_config['isotope_generation'])
 
-    assert ('0\t9\t+H\t0.0100\t1\t10000\t{100.000000}\t{1000.000000}\t'
+    assert list(isocalc_wrapper.formatted_iso_peaks(0, 9, 'Au', '+H'))[0] == \
+           ('0\t9\t+H\t0.010000\t1\t10000\t{100.000000}\t{1000.000000}\t'
             '{90.000000,100.000000,110.000000}\t'
-            '{1.000000,1000.000000,1001.000000}') == list(isocalc_wrapper.formatted_iso_peaks(0, 9, 'Au', '+H'))[0]
+            '{1.000000,1000.000000,1001.000000}')
 
 
 @mock.patch('engine.theor_peaks_gen.DB')
@@ -147,15 +146,15 @@ def test_generate_theor_peaks(mockDB, spark_context, sm_config, ds_config):
 
     peaks_gen = TheorPeaksGenerator(spark_context, sm_config, ds_config)
     peaks_gen.isocalc_wrapper.isotope_peaks = lambda *args: {'centr_mzs': [100.],
-                                                          'centr_ints': [1000.],
-                                                          'profile_mzs': [90., 100., 110.],
-                                                          'profile_ints': [1., 1000., 1001.]}
+                                                             'centr_ints': [1000.],
+                                                             'profile_mzs': [90., 100., 110.],
+                                                             'profile_ints': [1., 1000., 1001.]}
 
     sf_adduct_cand = [(9, 'Au', '+Na')]
     peak_lines = peaks_gen.generate_theor_peaks(sf_adduct_cand)
 
     assert len(peak_lines) == 1
-    assert peak_lines[0] == ('0\t9\t+Na\t0.0100\t1\t10000\t'
+    assert peak_lines[0] == ('0\t9\t+Na\t0.010000\t1\t10000\t'
                              '{100.000000}\t{1000.000000}\t'
                              '{90.000000,100.000000,110.000000}\t'
                              '{1.000000,1000.000000,1001.000000}')
