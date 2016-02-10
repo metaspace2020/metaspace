12997920e370e7eb01cbca123437658a164345aa
Dominik Fay <dominik.fay@gmx.de>
2016-02-10 19:17:08 +0100

    Use the blue rtd theme instead of the sphinx default

--- metaspace/engine/docs/conf.py
+++ metaspace/engine/docs/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 
 from mock import Mock
+import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
 
 sys.path.append('.')
@@ -152,7 +153,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -160,7 +161,7 @@ html_theme = 'alabaster'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
