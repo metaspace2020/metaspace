7110623c5eefbb08fb908f4a14226a2f9054e163
Vitaly Kovalev <vitaly.kovalev@embl.de>
2016-03-03 16:56:17 +0100

    Test and start script update. Not dataset owner support so far.

--- metaspace/engine/scripts/run_molecule_search.py
+++ metaspace/engine/scripts/run_molecule_search.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SM process dataset at a remote spark location.')
     parser.add_argument('ds_name', type=str, help='Dataset name')
     parser.add_argument('input_path', type=str, help='Path to a dataset location')
-    parser.add_argument('--email', type=str, help='Client email')
     parser.add_argument('--config', dest='sm_config_path', type=str, help='SM config path')
     parser.add_argument('--no-clean', dest='no_clean', action='store_true', help='do not clean interim files')
 
@@ -25,7 +24,7 @@ if __name__ == "__main__":
 
     logger.info("Processing...")
 
-    job = SearchJob(args.ds_name)
+    job = SearchJob(None, args.ds_name)
     job.run(args.input_path, clean=not args.no_clean)
 
     logger.info("All done!")
