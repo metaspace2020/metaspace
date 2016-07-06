c3f8a13772f7b36241ed6e14595557b27a720e77
Artem Tarasov <artem.tarasov@embl.de>
2016-07-06 11:14:45 +0200

    updated docker scripts

--- metaspace/engine/docker/.scripts/add_dataset_wrapper.sh
+++ metaspace/engine/docker/.scripts/add_dataset_wrapper.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 source /miniconda/bin/activate sm_distributed
 export PYTHONPATH=/code/SM_distributed:/root/spark-1.6.0-bin-hadoop2.6/python:$PYTHONPATH
-python scripts/run_molecule_search.py $1 /data/$2 --config /code/SM_distributed/docker/config.json
+python scripts/run_molecule_search.py $1 /data/$2 /data/$2/config.json --config /code/SM_distributed/docker/config.json
