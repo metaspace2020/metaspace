70518574ddae0d7e52d8bf35c787aa7d984ee32d
Vitaly Kovalev <intscorpio@gmail.com>
2016-08-15 15:21:27 +0200

    Scripts for consuming/putting messages (sm jobs) from/to rabbitmq

---
+++ metaspace/engine/scripts/add_sm_job_to_queue.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import argparse
+import pika
+import json
+
+from sm.engine.util import SMConfig
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script for adding messages to the job queue')
+    parser.add_argument('ds_name', type=str, help='Dataset name')
+    parser.add_argument('input_path', type=str, help='Path to a dataset location')
+    parser.add_argument('--config', dest='sm_config_path', default='conf/config.json', type=str, help='SM config path')
+
+    args = parser.parse_args()
+    SMConfig.set_path(args.sm_config_path)
+    rabbit_config = SMConfig.get_conf()['rabbitmq']
+
+    creds = pika.PlainCredentials(rabbit_config['user'], rabbit_config['password'])
+    conn = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_config['host'], credentials=creds))
+
+    ch = conn.channel()
+    ch.queue_declare(queue='sm_annotate')
+
+    m = {
+        'ds_name': args.ds_name,
+        'input_path': args.input_path
+    }
+
+    ch.basic_publish(exchange='', routing_key='sm_annotate', body=json.dumps(m))
+    print(" [x] Sent '{}'".format(json.dumps(m)))
+    conn.close()
---
+++ metaspace/engine/scripts/run_sm_job_daemon.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import argparse
+import os
+import pika
+import json
+from subprocess import check_output
+
+from sm.engine.util import SMConfig
+
+
+def run_job_callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+    job = json.loads(body)
+
+    print check_output(['python', '{}/scripts/run_molecule_search.py'.format(os.getcwd()),
+                        job['ds_name'], job['input_path']],
+                       cwd=os.getcwd())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Daemon for reading messages from the\ '
+                                                 'queue and starting annotation jobs')
+    parser.add_argument('--config', dest='sm_config_path', type=str, help='SM config path')
+
+    args = parser.parse_args()
+    SMConfig.set_path(args.sm_config_path)
+    rabbit_config = SMConfig.get_conf()['rabbitmq']
+
+    creds = pika.PlainCredentials(rabbit_config['user'], rabbit_config['password'])
+    conn = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_config['host'], credentials=creds))
+
+    ch = conn.channel()
+    ch.queue_declare(queue='sm_annotate')
+    ch.basic_consume(run_job_callback,
+                     queue='sm_annotate',
+                     no_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    ch.start_consuming()
