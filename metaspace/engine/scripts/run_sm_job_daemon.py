ebcebe4ca06b63685db8ab8ce9e1984d401990a3
Vitaly Kovalev <intscorpio@gmail.com>
2016-08-15 16:32:09 +0200

    bug fix: call run_molecule_search using scripts/run.sh

--- metaspace/engine/scripts/run_sm_job_daemon.py
+++ metaspace/engine/scripts/run_sm_job_daemon.py
@@ -13,14 +13,14 @@ def run_job_callback(ch, method, properties, body):
 
     job = json.loads(body)
 
-    print check_output(['python', '{}/scripts/run_molecule_search.py'.format(os.getcwd()),
+    print check_output(['scripts/run.sh', '{}/scripts/run_molecule_search.py'.format(os.getcwd()),
                         job['ds_name'], job['input_path']],
                        cwd=os.getcwd())
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Daemon for reading messages from the\ '
-                                                 'queue and starting annotation jobs')
+    parser = argparse.ArgumentParser(description=('Daemon for reading messages from the '
+                                                  'queue and starting annotation jobs'))
     parser.add_argument('--config', dest='sm_config_path', type=str, help='SM config path')
 
     args = parser.parse_args()
