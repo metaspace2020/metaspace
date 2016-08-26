1f68d79b7003ab41a6d4bc9e4d0504a0f7225e74
Vitaly Kovalev <intscorpio@gmail.com>
2016-08-26 16:29:38 +0200

    sm/webapp/webserver.py update: shebang, chmod +x
    * run_molecule_search.py: --ds-id parameter

--- metaspace/engine/scripts/run_molecule_search.py
+++ metaspace/engine/scripts/run_molecule_search.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SM process dataset at a remote spark location.')
     parser.add_argument('input_path', type=str, help='Path to a dataset location')
     parser.add_argument('--ds-name', dest='ds_name', type=str, help='Dataset name')
+    parser.add_argument('--ds-id', dest='ds_id', type=str, help='Dataset id')
     parser.add_argument('--ds-config', dest='ds_config_path', type=str, help='Path to a dataset config file')
     parser.add_argument('--config', dest='sm_config_path', type=str, help='SM config path')
 
@@ -21,7 +22,7 @@ if __name__ == "__main__":
 
     init_logger()
 
-    ds_id = dt.now().strftime("%Y-%m-%d_%Hh%Mm")
+    ds_id = args.ds_id or dt.now().strftime("%Y-%m-%d_%Hh%Mm")
     job = SearchJob(ds_id, args.ds_name, args.input_path, args.sm_config_path)
     try:
         job.run(args.ds_config_path)
