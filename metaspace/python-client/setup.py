bf3fc92557a6dc03449bb0414f175ee29cf40573
andy-d-palmer <palmer@embl.de>
2016-09-07 16:54:15 +0200

    packaging for pip install

--- metaspace/python-client/setup.py
+++ metaspace/python-client/setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
-from pyMSpec import __version__
+from sm_annotation_utils import __version__
 
-setup(name='pyMSpec',
+setup(name='sm_annotation_utils',
       version=__version__,
-      description='Python library for processing individual mass spectra',
-      url='https://github.com/alexandrovteam/pyMSpec',
+      description='Python library for working with sm-engine annotations',
+      url='https://github.com/spatialmetaboloimcs/sm_engine_utils',
       author='Alexandrov Team, EMBL',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[ 'elasticsearch',
+                         'elasticsearch_dsl',
+                         'pandas',
+                         'plotly',
+                         'numpy',
+            ])
