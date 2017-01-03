3f9b54503a2abd71a1a4413a5d20e8c0afe21d9a
Vitaly Kovalev <intscorpio@gmail.com>
2017-01-03 19:24:56 +0100

    spark version upgrade in docker files, test coverage badge

--- metaspace/engine/docker/env.sh
+++ metaspace/engine/docker/env.sh
@@ -1,3 +1,3 @@
 source activate sm_engine
 pip install -q /code/sm-engine
-export PYTHONPATH=.:/root/spark-1.6.0-bin-hadoop2.6/python:$PYTHONPATH
+export PYTHONPATH=.:/root/spark-2.0.2-bin-hadoop2.7/python:$PYTHONPATH
