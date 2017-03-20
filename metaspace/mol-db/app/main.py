22aa334e33bf07779c9ed85b149fd6f02ad49c28
Vitaly Kovalev <kovalev@embl.de>
2017-03-20 15:51:05 +0100

    remove dev wsgi server (replaced by gunicorn)

--- metaspace/mol-db/app/main.py
+++ metaspace/mol-db/app/main.py
@@ -42,9 +42,3 @@ middleware = [
     DatabaseSessionManager(db_session)
 ]
 application = App(middleware=middleware)
-
-
-if __name__ == "__main__":
-    from wsgiref import simple_server
-    httpd = simple_server.make_server('127.0.0.1', 5000, application)
-    httpd.serve_forever()
