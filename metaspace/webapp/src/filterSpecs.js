39421f8d899148a09e631f08b2b7f21f439abe91
Artem Tarasov <artem.tarasov@embl.de>
2017-04-12 16:53:57 +0000

    add 0.5 fdr filter option

--- metaspace/webapp/src/filterSpecs.js
+++ metaspace/webapp/src/filterSpecs.js
@@ -121,7 +121,7 @@ const FILTER_SPECIFICATIONS = {
     levels: ['annotation'],
     initialValue: 0.1,
 
-    options: ['0.05', '0.1', '0.2'],
+    options: ['0.05', '0.1', '0.2', '0.5'],
     filterable: false
   },
 
