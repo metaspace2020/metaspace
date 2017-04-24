c4c17e44653a8ed18ea702e12a7a8c2f489a82b4
Artem Tarasov <artem.tarasov@embl.de>
2017-04-24 22:03:34 +0200

    wait the page 10s

--- metaspace/webapp/tests/e2e/annotations.js
+++ metaspace/webapp/tests/e2e/annotations.js
@@ -2,7 +2,7 @@ import config from '../../conf';
 
 import {Selector} from 'testcafe';
 
-const DATA_WAIT = 5000;
+const DATA_WAIT = 10000;
 
 fixture `Annotations page`
   .page `http://${config.HOST_NAME}:${config.PORT}/#/annotations`;
