8fdd4bde5b5a7e0f7e5319343aa39b904ad127dd
Artem Tarasov <artem.tarasov@embl.de>
2017-05-30 14:04:17 +0000

    make m/z filter behavior less confusing

--- metaspace/webapp/src/util.js
+++ metaspace/webapp/src/util.js
@@ -62,10 +62,10 @@ function createColormap(name) {
 function mzFilterPrecision(value) {
   const splitVal = (value + '').split('.');
   if (splitVal.length == 1) {
-    return 0.5;
+    return 1.0;
   } else {
     const k = splitVal[1].length;
-    return (0.5 * Math.pow(0.1, k)).toFixed(k + 1);
+    return (1.0 * Math.pow(0.1, k)).toFixed(k + 1);
   }
 }
 
