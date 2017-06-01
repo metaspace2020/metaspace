b299653d61666caaaed13c5bbefec6e8a3cf0318
Artem Tarasov <artem.tarasov@embl.de>
2017-06-01 15:43:32 +0000

    fixed py3 tests

--- metaspace/engine/docker/env.sh
+++ metaspace/engine/docker/env.sh
@@ -1,3 +1,5 @@
 source activate sm_engine
-pip install -q /code/sm-engine
+cd /code/sm-engine
+pip install -U --upgrade-strategy only-if-needed git+https://github.com/alexandrovteam/pyImagingMSpec
+pip install -U --upgrade-strategy only-if-needed .
 export PYTHONPATH=.:/root/spark-2.0.2-bin-hadoop2.7/python:$PYTHONPATH
