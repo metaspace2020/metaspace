2924009025676efd06281451fcb64ca2cac2e16b
Artem Tarasov <artem.tarasov@embl.de>
2017-06-20 17:46:34 +0200

    use CircleCI instead of Travis CI
    because circleci build is awesome

--- metaspace/engine/docker/env.sh
+++ metaspace/engine/docker/env.sh
@@ -1,5 +1,4 @@
 source activate sm_engine
 cd /code/sm-engine
-pip install -U --upgrade-strategy only-if-needed git+https://github.com/alexandrovteam/pyImagingMSpec
 pip install -U --upgrade-strategy only-if-needed .
-export PYTHONPATH=.:/root/spark-2.0.2-bin-hadoop2.7/python:$PYTHONPATH
+export PYTHONPATH=.:/root/spark-2.1.1-bin-hadoop2.7/python:$PYTHONPATH
