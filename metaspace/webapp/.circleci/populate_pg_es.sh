ff99540c9f7d602de01f1ba8c87aa1945b0bd641
Artem Tarasov <artem.tarasov@embl.de>
2017-07-06 11:55:07 +0200

    fix tests (updated ES test index dump)

--- metaspace/webapp/.circleci/populate_pg_es.sh
+++ metaspace/webapp/.circleci/populate_pg_es.sh
@@ -1,12 +1,11 @@
 mkdir -p tmp
 cd tmp
 
-DUMPS=https://s3-eu-west-1.amazonaws.com/embl-sm-testing/dumps/2017-06-29
-curl -s $DUMPS/pg_dump | PGPASSWORD=password pg_restore -U sm -h localhost --dbname=sm
+DUMPS=https://s3-eu-west-1.amazonaws.com/embl-sm-testing/dumps/2017-07-06
 
-wget -q $DUMPS/sm_index_settings.json.gz;
+wget -q $DUMPS/sm_index_settings.json.gz
 gunzip -f sm_index_settings.json.gz
 curl -s -XPUT http://localhost:9200/sm -d @sm_index_settings.json
 
 wget -qN $DUMPS/sm_index_data.json.gz
-zcat sm_index_data.json.gz | elasticdump --input=$ --output=http://localhost:9200/sm --type=$x
+zcat sm_index_data.json.gz | elasticdump --input=$ --output=http://localhost:9200/sm
