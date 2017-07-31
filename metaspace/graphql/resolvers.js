bc332d02c4517f3cd4a035e0fcb098fb98fb2ebe
Vitaly Kovalev <kovalev@embl.de>
2017-07-31 19:12:38 +0200

    mutations in sync/async mode, priority default value 0

--- metaspace/graphql/resolvers.js
+++ metaspace/graphql/resolvers.js
@@ -321,7 +321,7 @@ const Resolvers = {
 
   Mutation: {
     submitDataset(_, args) {
-      const {name, path, metadataJson, datasetId, priority} = args;
+      const {name, path, metadataJson, datasetId, priority, sync} = args;
       try {
         const payload = jwt.decode(args.jwt, config.jwt.secret);
 
@@ -332,11 +332,11 @@ const Resolvers = {
           input_path: path,
           metadata: metadata,
           config: generateProcessingConfig(metadata),
-          priority: priority !== undefined ? priority : 0
+          priority: priority
         });
 
         const url = `http://${config.services.sm_engine_api_host}/v1/datasets/add`;
-        return fetch(url, {
+        let smAPIPromise = fetch(url, {
              method: 'POST',
              body: body,
              headers: {
@@ -347,6 +347,8 @@ const Resolvers = {
             logger.error(`${e.message}\n`);
             return e.message
           });
+        if (sync)
+          return smAPIPromise;
       } catch (e) {
         logger.error(e);
         return e.message;
@@ -354,7 +356,7 @@ const Resolvers = {
     },
 
     resubmitDataset(_, args) {
-      const {datasetId, priority} = args;
+      const {datasetId, priority, sync} = args;
       try {
         const payload = jwt.decode(args.jwt, config.jwt.secret);
 
@@ -369,13 +371,15 @@ const Resolvers = {
                   input_path: ds.input_path,
                   metadata: ds.metadata,
                   config: ds.config,
-                  priority: priority !== undefined ? priority : 0
+                  priority: priority
                 });
 
                 const url = `http://${config.services.sm_engine_api_host}/v1/datasets/add`;
-                return fetch(url, { method: 'POST', body: body, headers: {
+                let smAPIPromise = fetch(url, { method: 'POST', body: body, headers: {
                   "Content-Type": "application/json"
                 }});
+                if (sync)
+                  return smAPIPromise;
               });
           })
           .then(() => "success")
@@ -390,7 +394,7 @@ const Resolvers = {
     },
 
     updateMetadata(_, args) {
-      const {datasetId, metadataJson, priority} = args;
+      const {datasetId, metadataJson, priority, sync} = args;
       try {
         const payload = jwt.decode(args.jwt, config.jwt.secret);
         const newMetadata = JSON.parse(metadataJson);
@@ -402,7 +406,7 @@ const Resolvers = {
               metadata: newMetadata,
               config: generateProcessingConfig(newMetadata),
               name: newMetadata.metaspace_options.Dataset_Name || "",
-              priority: priority !== undefined ? priority : 0
+              priority: priority
             });
 
             return pg.select().from('dataset').where('id', '=', datasetId)
@@ -413,13 +417,15 @@ const Resolvers = {
               .then(() => {
                 // perform ES re-indexing in the background
                 const url = `http://${config.services.sm_engine_api_host}/v1/datasets/${datasetId}/update`;
-                fetch(url, { method: 'POST', body: body, headers: {
+                let smAPIPromise = fetch(url, { method: 'POST', body: body, headers: {
                     "Content-Type": "application/json"
                   }})
                   .catch( e => {
                     logger.error(`metadata update error: ${e.message}\n${e.stack}`);
                     metadataUpdateFailedSlackNotify(user, datasetId, e.message);
                   });
+                if (sync)
+                  return smAPIPromise;
               })
               .then(() => "success");
           })
@@ -434,7 +440,7 @@ const Resolvers = {
     },
 
     deleteDataset(_, args) {
-      const {datasetId, delRawData} = args;
+      const {datasetId, delRawData, sync} = args;
 
       try {
         const payload = jwt.decode(args.jwt, config.jwt.secret);
@@ -446,7 +452,9 @@ const Resolvers = {
             //   body = JSON.stringify({});
             // else
             //   body = JSON.stringify({ "del_raw": true });
-            return fetch(url, {method: "POST", body: body});
+            let smAPIPromise = fetch(url, {method: "POST", body: body});
+            if (sync)
+              return smAPIPromise;
           })
           .then(res => res.statusText)
           .catch(e => {
