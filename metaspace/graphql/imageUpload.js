c72fc0db4467c32e8326d0991ba7ec018eaaa0d1
Vitaly Kovalev <kovalev@embl.de>
2017-08-08 19:26:13 +0200

    imageUpload - create iso_image if it doesn't exist yet

--- metaspace/graphql/imageUpload.js
+++ metaspace/graphql/imageUpload.js
@@ -14,54 +14,59 @@ function imageProviderDBBackend(app) {
   let storage = multer.memoryStorage();
   let upload = multer({ storage: storage });
 
-  app.get(path.join(config.img_upload.img_base_path, ":img_id"),
-    function (req, res) {
-      pg.select(pg.raw('data'))
-        .from('iso_image')
-        .where('id', '=', req.params.img_id)
-        .first()
-        .then((row) => {
-          if (row === undefined)
-            throw ({message: `Image with id=${image_id} does not exist`});
-          let img_buf = row.data;
-          res.type('image/png');
-          res.end(img_buf, 'binary');
-        })
-        .catch((e) => {
-          logger.error(e.message);
-          res.status(404).send('Not found');
-        });
-    });
+  pg.schema.createTableIfNotExists('iso_image', function (table) {
+    table.text('id');
+    table.binary('data');
+  }).then(() => {
+    app.get(path.join(config.img_upload.img_base_path, ":img_id"),
+      function (req, res) {
+        pg.select(pg.raw('data'))
+          .from('iso_image')
+          .where('id', '=', req.params.img_id)
+          .first()
+          .then((row) => {
+            if (row === undefined)
+              throw ({message: `Image with id=${image_id} does not exist`});
+            let img_buf = row.data;
+            res.type('image/png');
+            res.end(img_buf, 'binary');
+          })
+          .catch((e) => {
+            logger.error(e.message);
+            res.status(404).send('Not found');
+          });
+      });
 
-  app.post(path.join(config.img_upload.img_base_path, 'upload'), upload.single('iso_image'),
-    function (req, res, next) {
-      logger.debug(req.file.originalname);
-      let img_id = crypto.randomBytes(16).toString('hex');
+    app.post(path.join(config.img_upload.img_base_path, 'upload'), upload.single('iso_image'),
+      function (req, res, next) {
+        logger.debug(req.file.originalname);
+        let img_id = crypto.randomBytes(16).toString('hex');
 
-      let img_buf = req.file.buffer;
-      pg.insert({'id': img_id, 'data': img_buf})
-        .into('iso_image')
-        .then((m) => {
-          logger.debug(`${m}`);
-          res.status(201).json({ image_id: img_id });
-        })
-        .catch((e) => {
-          logger.error(e.message);
-          res.status(500).send('Failed to store image');
-        });
-    });
+        let img_buf = req.file.buffer;
+        pg.insert({'id': img_id, 'data': img_buf})
+          .into('iso_image')
+          .then((m) => {
+            logger.debug(`${m}`);
+            res.status(201).json({ image_id: img_id });
+          })
+          .catch((e) => {
+            logger.error(e.message);
+            res.status(500).send('Failed to store image');
+          });
+      });
 
-  app.delete(path.join(config.img_upload.img_base_path, 'delete', ":img_id"),
-    function (req, res, next) {
-      pg.del().from('iso_image')
-        .where('id', '=', req.params.img_id)
-        .catch((e) => {
-          logger.error(e.message);
-        })
-        .then((m) => {
-          logger.debug(`${m}`);
-          res.status(202).end();
-        })
+    app.delete(path.join(config.img_upload.img_base_path, 'delete', ":img_id"),
+      function (req, res, next) {
+        pg.del().from('iso_image')
+          .where('id', '=', req.params.img_id)
+          .catch((e) => {
+            logger.error(e.message);
+          })
+          .then((m) => {
+            logger.debug(`${m}`);
+            res.status(202).end();
+          })
+      });
     });
 }
 
