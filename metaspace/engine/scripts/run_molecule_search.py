7e20ea60710af37428ff978a2013e65e233d241e
Vitaly Kovalev <kovalev@embl.de>
2018-01-10 15:44:37 +0100

    run molecule search script bug fix

--- metaspace/engine/scripts/run_molecule_search.py
+++ metaspace/engine/scripts/run_molecule_search.py
@@ -8,6 +8,7 @@ import sys
 from sm.engine import DB
 from sm.engine import ESExporter
 from sm.engine import SMDaemonDatasetManager
+from sm.engine.png_generator import ImageStoreServiceWrapper
 from sm.engine.util import SMConfig, logger, sm_log_config, init_logger, create_ds_from_files
 from sm.engine.search_job import SearchJob
 
@@ -27,7 +28,8 @@ if __name__ == "__main__":
     SMConfig.set_path(args.sm_config_path)
     sm_config = SMConfig.get_conf()
     db = DB(sm_config['db'])
-    ds_man = SMDaemonDatasetManager(db, ESExporter(db), mode='local')
+    img_store = ImageStoreServiceWrapper(sm_config['services']['img_service_url'])
+    ds_man = SMDaemonDatasetManager(db, ESExporter(db), img_store, mode='local')
 
     try:
         ds = create_ds_from_files(args.ds_id, args.ds_name, args.input_path)
