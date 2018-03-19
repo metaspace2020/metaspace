58778978bff6b09314201059ecbf8da685a68ef4
Ivan Protsyuk <iprotsyuk@gmail.com>
2018-03-19 14:27:47 +0100

    Typo fix

--- metaspace/engine/scripts/set_md_type.py
+++ metaspace/engine/scripts/set_md_type.py
@@ -22,7 +22,7 @@ def set_metadata_type(db, md_type, ds_name):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Script for deleting annotation results on per dataset basis')
+    parser = argparse.ArgumentParser(description='Script for updating metadata in DB on per dataset basis')
     parser.add_argument('--md-type', dest='md_type', type=str, help='Target metadata type')
     parser.add_argument('--ds-name', dest='ds_name', type=str, help="DS name prefix mask ({} for all datasets)".format(ALL_DS_MASK))
     parser.add_argument('--config', dest='sm_config_path', default='conf/config.json', type=str, help='SM config path')
