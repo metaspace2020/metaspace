2d0308ccc091004d25abf320b66ffa254aa88d28
Vitaly Kovalev <kovalev@embl.de>
2018-03-29 16:58:10 +0200

    sci test report update (fixed bug in iso img gen code), test_runner update

--- metaspace/engine/scripts/test_runner.py
+++ metaspace/engine/scripts/test_runner.py
@@ -30,15 +30,7 @@ if __name__ == '__main__':
 
         # Regression tests
         if args.regr or args.all:
-            local(py_test_cmd + 'tests/test_imzml_txt_converter_db.py')
-            local(py_test_cmd + 'tests/test_ion_centroids_gen.py')
-            local(py_test_cmd + 'tests/test_work_dir.py')
-            # TODO: include the test in CI
-            # local(py_test_cmd + 'tests/test_search_job.py')
-            local(py_test_cmd + 'tests/test_dataset_manager.py')
-            local(py_test_cmd + 'tests/test_search_results.py')
-            local(py_test_cmd + 'tests/test_es_exporter.py')
-            local(py_test_cmd + 'tests/test_z_search_job_imzml_example.py')
+            local(py_test_cmd + 'tests')
 
         # Functional/scientific tests
         if args.sci or args.all:
