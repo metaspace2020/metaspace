5f6150db19e07067da3db02603aa7b457af89112
Lachlan Stuart <Lachlan@LachlanStuart.com>
2018-05-30 10:35:52 +0200

    Include AboutPage in initial bundle Prefetch other chunks

--- metaspace/webapp/src/router.ts
+++ metaspace/webapp/src/router.ts
@@ -1,31 +1,32 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import AboutPage from './components/AboutPage.vue';
 import DatasetsPage from './components/DatasetsPage.vue';
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/about' },
-    { path: '/annotations', component: async () => await import(/* webpackChunkName: "AnnotationsPage" */ './components/AnnotationsPage.vue') },
+    { path: '/annotations', component: async () => await import(/* webpackPrefetch: true, webpackChunkName: "AnnotationsPage" */ './components/AnnotationsPage.vue') },
     {
       path: '/datasets',
       component: DatasetsPage,
       children: [
-        {path: '', component: async () => await import(/* webpackChunkName: "DatasetTable" */ './components/DatasetTable.vue')},
-        {path: 'edit/:dataset_id', component: async () => await import(/* webpackChunkName: "MetadataEditPage" */ './components/MetadataEditPage.vue'), name: 'edit-metadata'},
+        {path: '', component: async () => await import(/* webpackPrefetch: true, webpackChunkName: "DatasetTable" */ './components/DatasetTable.vue')},
+        {path: 'edit/:dataset_id', component: async () => await import(/* webpackPrefetch: true, webpackChunkName: "MetadataEditPage" */ './components/MetadataEditPage.vue'), name: 'edit-metadata'},
         {
           path: ':dataset_id/add-optical-image',
           name: 'add-optical-image',
-          component: async () => await import(/* webpackChunkName: "ImageAlignmentPage" */ './components/ImageAlignmentPage.vue')
+          component: async () => await import(/* webpackPrefetch: true, webpackChunkName: "ImageAlignmentPage" */ './components/ImageAlignmentPage.vue')
         }
       ]
     },
     {
       path: '/upload',
-      component: async () => await import(/* webpackChunkName: "UploadPage" */ './components/UploadPage.vue')
+      component: async () => await import(/* webpackPrefetch: true, webpackChunkName: "UploadPage" */ './components/UploadPage.vue')
     },
-    { path: '/about', component: async () => await import(/* webpackChunkName: "AboutPage" */ './components/AboutPage.vue') },
-    { path: '/help', component: async () => await import(/* webpackChunkName: "HelpPage" */ './components/HelpPage.vue') }
+    { path: '/about', component: AboutPage },
+    { path: '/help', component: async () => await import(/* webpackPrefetch: true, webpackChunkName: "HelpPage" */ './components/HelpPage.vue') }
   ]
 })
 
