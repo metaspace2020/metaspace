62b4f53ba44e0073f4079fe24b821340f0fd2f24
Lachlan Stuart <Lachlan@LachlanStuart.com>
2018-08-16 16:12:46 +0200

    Start adding CircleCI config to generate the graphql schema before running webapp tests Disabled failing webapp e2e tests

---
+++ metaspace/graphql/bin/generate-json-graphql-schema.js
@@ -0,0 +1,30 @@
+const {makeExecutableSchema} = require('graphql-tools');
+const {mergeTypes} = require('merge-graphql-schemas');
+const {graphql, introspectionQuery, printSchema} = require('graphql');
+const {promisify} = require('util');
+const {resolve} = require('path');
+const readFile = promisify(require("fs").readFile);
+const writeFile = promisify(require("fs").writeFile);
+
+const schemaFiles = [
+  '../schema.graphql',
+  '../schemas/user.graphql',
+  '../schemas/group.graphql',
+];
+
+const run = async (outputFile) => {
+  const filePromises = schemaFiles.map(file => readFile(resolve(__dirname, file), 'utf8'));
+  const typeDefs = mergeTypes(await Promise.all(filePromises));
+  const schema = makeExecutableSchema({typeDefs});
+  const introspectedSchema = await graphql(schema, introspectionQuery);
+  await writeFile(outputFile, JSON.stringify(introspectedSchema));
+  // For .graphql schema:
+  // await writeFile(outputFile, printSchema(introspectedSchema));
+};
+
+
+if (process.argv.length !== 3) {
+  throw 'Run this script with just 1 argument: the output file path';
+} else {
+  run(process.argv[2]).catch(console.error);
+}
