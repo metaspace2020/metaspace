68554cf968c2cc30cbeba5b73a429ee11c402c89
Vitaly Kovalev <kovalev@embl.de>
2018-08-28 14:02:12 +0200

    user module, refactored out hasAccess function

--- metaspace/graphql/src/modules/user/operation.ts
+++ metaspace/graphql/src/modules/user/operation.ts
@@ -1,87 +1,80 @@
-import {User as UserModel} from './model';
+import {User as UserModel, Dataset} from './model';
 import {Repository, Like} from 'typeorm';
 import {UpdateUserInput} from '../../binding';
 import {UserError} from 'graphql-errors';
 
-export const generateUserOperations = (user: UserModel, userRepo: Repository<UserModel>) => ({
+function hasAccess(user: UserModel, userId?: string) {
+  if (!user
+    || (user.role !== 'admin' && userId && user.id !== userId))
+    throw new UserError('Access denied');
+}
 
-  findUserById: async (id: string) => {
-    if (user && (user.role === 'admin' || user.id === id)) {
-      if (id) {
-        const userObj = await userRepo.findOne({
-          relations: ['credentials'],
-          where: { 'id': id }
-        });
+export const generateUserOperations = (user: UserModel,
+                                       userRepo: Repository<UserModel>,
+                                       dsRepo: Repository<Dataset>) => ({
+  findUserById: async (userId: string) => {
+    hasAccess(user, userId);
 
-        if (userObj) {
-          return {
-            id: userObj.id,
-            name: userObj.name,
-            role: userObj.role,
-            email: userObj.email
-          }
+    if (userId) {
+      const userObj = await userRepo.findOne({
+        relations: ['credentials'],
+        where: { 'id': userId }
+      });
+
+      if (userObj) {
+        return {
+          id: userObj.id,
+          name: userObj.name,
+          role: userObj.role,
+          email: userObj.email
         }
       }
-    }
-    else {
-      throw new UserError('Access denied');
     }
   },
 
   findUsers: async (query: string='') => {
-    if (user && user.role === 'admin') {
-      return await userRepo.find({
-        where: {
-          name: Like(`%${query}%`)
-        }
-      });
-    }
-    else {
-      throw new UserError('Access denied');
-    }
+    hasAccess(user);
+
+    return await userRepo.find({
+      where: {
+        'name': Like(`%${query}%`)
+      }
+    });
   },
 
-  updateUser: async (id: string, update: UpdateUserInput) => {
-    if (user && (user.role === 'admin' || user.id === id)) {
+  updateUser: async (userId: string, update: UpdateUserInput) => {
+    hasAccess(user, userId);
 
-      if (update.role && user.role !== 'admin') {
-        throw new UserError('Only admin can update role');
-      }
-
-      if (update.email) {
-        // TODO: confirm new email
-        throw new UserError('Not implemented yet');
-      }
-
-      let userObj = await userRepo.findOne(id);
-      if (userObj) {
-        userObj = Object.assign(userObj, update);
-        await userRepo.save(userObj);
-      }
-      return userObj;
+    if (update.role && user.role !== 'admin') {
+      throw new UserError('Only admin can update role');
     }
-    else {
-      throw new UserError('Access denied');
+
+    if (update.email) {
+      // TODO: confirm new email
+      throw new UserError('Not implemented yet');
     }
+
+    let userObj = await userRepo.findOne(userId);
+    if (userObj) {
+      userObj = Object.assign(userObj, update);
+      await userRepo.save(userObj);  // update by id doesn't work properly
+    }
+    return userObj;
   },
 
-  deleteUser: async (id: string, deleteDatasets: boolean=false) => {
-    if (user && (user.role === 'admin' || user.id === id)) {
+  deleteUser: async (userId: string, deleteDatasets: boolean=false) => {
+    hasAccess(user, userId);
 
-      if (deleteDatasets) {
-        throw new UserError('Not implemented yet');
-      }
+    if (deleteDatasets) {
+      throw new UserError('Not implemented yet');
+    }
 
-      let userObj = await userRepo.findOne(id);
-      if (!userObj) {
-        throw new UserError('User does not exist');
-      }
-      await userRepo.delete(id);
-      // TODO: delete credentials
-      return true;
+    let userObj = await userRepo.findOne(userId);
+    if (!userObj) {
+      throw new UserError('User does not exist');
     }
-    else {
-      throw new UserError('Access denied');
-    }
+    await userRepo.delete(userId);
+    // TODO: delete credentials
+    return true;
   },
 });
