959271bdee8d312f555d68f9b5e805fa9d27324f
Ren22 <ngm.renat@gmail.com>
2018-09-11 16:13:07 +0200

    Added a dialog for help button with information from the previous welcome message(but curtailed) Added external link class (will add an icon to open in a new window) Modified dialog to look for a project on the upload page, so the dialog window doesn't jump to the top when clicked.

--- metaspace/webapp/src/modules/MetadataEditor/UploadPage.vue
+++ metaspace/webapp/src/modules/MetadataEditor/UploadPage.vue
@@ -4,8 +4,17 @@
       :visible.sync="helpDialog"
       :lock-scroll="false"
       append-to-body>
-      <embed :src="helpLink"
-             style="width:800px; height: 800px;">
+      <p>Thank you for considering submitting your data to METASPACE! Here are the key points you need to know:</p>
+      <p style="padding-left: 15px"><b>Type of MS:</b> We can annotate only FTICR- or Orbitrap- imaging MS data.</p>
+      <p style="padding-left: 15px"><b>Format:</b> We can receive only data in the imzML centroided format.
+        Please check out
+        <a :href="helpLink" target="_blank" class="external" title="The page will be opened in a new page">our instructions</a>
+        for converting datasets into this format. If you are experiencing difficulties,
+        please contact your instrument vendor.</p>
+      <p>If you have any further questions, please check out our main
+        <a href="#/help" target="_blank" class="external" title="The page will be opened in a new page">help</a>
+        page or email us at <a href="mailto:contact@metaspace2020.eu">contact@metaspace2020.eu</a></p>
+      <p>Have fun using METASPACE!</p>
     </el-dialog>
     <div class="upload-page-wrapper">
       <div v-if="!enableUploads">
@@ -29,13 +38,13 @@
                          style="flex-basis: 80%"
                          @upload="onUpload" @success="onUploadSuccess" @failure="onUploadFailure" />
           <div class="md-editor-submit">
-            <el-button type="info" class="el-button__help_metadata" @click="helpDialog = true" icon="el-icon-question"></el-button>
+            <el-button type="info" @click="helpDialog=true" class="el-button__help_metadata" icon="el-icon-question"></el-button>
             <el-button v-if="enableSubmit" type="primary" @click="onSubmit" class="el-button__submit_metadata">Submit</el-button>
             <el-button v-else type="primary" disabled :title="disabledSubmitMessage" class="el-button__submit_metadata">Submit</el-button>
           </div>
-        </div>
 
-        <metadata-editor ref="editor":validationErrors="validationErrors" />
+        </div>
+        <metadata-editor ref="editor" :validationErrors="validationErrors" />
       </div>
     </div>
   </div>
@@ -117,7 +126,7 @@
        uploadedUuid: null,
        features: config.features,
 	     helpDialog: false,
-       helpLink: "https://docs.google.com/document/d/1gKlhxWlY0WTZn7uLIcUxfRJGQVYEwFCRXfVZqTbvhA0/edit?usp=sharing"
+       helpLink: "https://docs.google.com/document/d/e/2PACX-1vTT4QrMQ2RJMjziscaU8S3gbznlv6Rm5ojwrsdAXPbR5bt7Ivp-ThkC0hefrk3ZdVqiyCX7VU_ddA62/pub"
      }
    },
    components: {
@@ -288,28 +297,32 @@
   .md-editor-submit {
     display: flex;
     flex-direction: row;
-    flex: 1;
     justify-content: space-between;
-    padding: 5px 0;
   }
 
   .md-editor-submit > button {
     flex: 1;
-    margin: auto 5px;
-    font-size: 150%;
+    margin: 25px 5px;
   }
 
   .el-button__submit_metadata {
     padding: 20px;
+    font-size: 150%;
   }
 
   .el-button__help_metadata {
-    padding: 20px 0;
+    padding: 20px;
+    font-size: 150%;
   }
 
   .fine-uploader-wrapper {
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
+  }
+
+  a[target="_blank"]:after {
+    content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==);
+    margin: 0 2px;
   }
 </style>
