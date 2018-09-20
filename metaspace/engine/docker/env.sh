97b4072706aef645d00567e888c029fd27eb29f1
Vitaly Kovalev <kovalev@embl.de>
2018-09-20 17:49:08 +0200

    bug fix: updated pyimzML, install pyimzML from github
    * ansible metaspace_repo role - upload local files for engine from root dir
    * docker env update for circleci

--- metaspace/engine/docker/env.sh
+++ metaspace/engine/docker/env.sh
@@ -1,4 +1,5 @@
 source activate sm_engine
 cd ~/metaspace/metaspace/engine
-pip install -U --upgrade-strategy only-if-needed .
+pip install -U pip
+pip install -U --upgrade-strategy only-if-needed --process-dependency-links .
 export PYTHONPATH=.:/root/spark-2.3.0-bin-hadoop2.7/python:$PYTHONPATH
