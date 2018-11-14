78fc6410a8e4d171e56a7ac2fed452282ae5a4a1
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2018-11-14 15:01:24 +0100

    Remove connection pool as it effectively didn't do anything

--- metaspace/engine/sm/engine/dataset_locker.py
+++ metaspace/engine/sm/engine/dataset_locker.py
@@ -1,28 +1,23 @@
 from contextlib import contextmanager
-from psycopg2.pool import ThreadedConnectionPool
+from psycopg2 import connect
 from zlib import adler32
 
 
 LOCK_KEY = 894951
-MIN_CONNS = 0
-MAX_CONNS = 2
 
 class DatasetLocker(object):
     def __init__(self, dbconfig):
-        self._pool = ThreadedConnectionPool(MIN_CONNS, MAX_CONNS, **dbconfig)
+        self._dbconfig = dbconfig
 
     @contextmanager
     def lock(self, ds_id, timeout=60):
         hash = adler32(ds_id.encode('utf-8'))
-        conn = self._pool.getconn()
+        conn = connect(**self._dbconfig)
         try:
             with conn.cursor() as curs:
                 curs.execute('SET statement_timeout = %s;', (timeout * 1000,))
                 curs.execute('SELECT pg_advisory_lock(%s, %s);', (LOCK_KEY, hash))
                 yield None
                 curs.execute('SELECT pg_advisory_unlock(%s, %s);', (LOCK_KEY, hash))
-        except:
-            self._pool.putconn(conn, close=True)
-            raise
-        else:
-            self._pool.putconn(conn)
+        finally:
+            conn.close()
