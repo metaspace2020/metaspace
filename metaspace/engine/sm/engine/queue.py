bb542bfce197f9ec4ea23669beeba622dc1f1c54
Vitaly Kovalev <kovalev@embl.de>
2019-01-03 10:50:38 +0100

    message queue - exit action support

--- metaspace/engine/sm/engine/queue.py
+++ metaspace/engine/sm/engine/queue.py
@@ -394,6 +394,10 @@ class QueueConsumer(Thread):
                                  method.delivery_tag, properties.app_id, body)
                 msg = json.loads(body)
 
+                if msg['action'] == 'exit':
+                    self.stop()
+                    return
+
                 self._callback(msg)
             except BaseException as e:
                 self.logger.error(' [x] Failed: {}'.format(body), exc_info=False)
