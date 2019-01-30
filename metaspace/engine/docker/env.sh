e9aea24fba2c44268f7b47e39b3cb604f554986e
Vitaly Kovalev <kovalev@embl.de>
2019-01-30 18:11:23 +0100

    paths changed inside engine docker image
    * moved out engine dependencies installation from metaspace/engine/docker/env.sh

--- metaspace/engine/docker/env.sh
+++ metaspace/engine/docker/env.sh
@@ -3,7 +3,3 @@ chmod 0600 ~/.pgpass
 
 mkdir -p logs
 cp docker/sci_test_config.json conf/config.json
-
-pip install --upgrade -r requirements.txt
-pip install -e .
-export PYTHONPATH=.:/root/spark-2.3.0-bin-hadoop2.7/python:$PYTHONPATH
