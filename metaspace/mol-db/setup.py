21098fd0a4b1de7f106db1ecf524534eb8838e8b
Vitaly Kovalev <kovalev@embl.de>
2019-02-04 12:55:17 +0100

    mol-db, removed requirements.txt, specified minimal dep requirements in setup.py

--- metaspace/mol-db/setup.py
+++ metaspace/mol-db/setup.py
@@ -10,10 +10,10 @@ setup(name='mol_db',
       author_email='vitaly.kovalev@embl.de',
       packages=find_packages(),
       install_requires=[
-          "falcon==1.1.0",
-          "sqlalchemy==1.1.5",
-          "psycopg2==2.6.2",
-          "pandas==0.19.2",
-          "cpyMSpec==0.3.4",
-          "pyMSpec==0.1.2"
+          "falcon>=1.1.0",
+          "sqlalchemy>=1.1.5",
+          "psycopg2>=2.6.2",
+          "pandas>=0.19.2",
+          "cpyMSpec>=0.3.4",
+          "pyMSpec>=0.1.2"
       ])
