aa1b67a0d15450c06a440cf0a2933c61e98fe2c1
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2019-02-07 10:08:50 +0100

    Use pathlib instead of os.path

--- metaspace/engine/sm/engine/tests/graphql_sql_schema.py
+++ metaspace/engine/sm/engine/tests/graphql_sql_schema.py
@@ -1,13 +1,12 @@
-import os
-
-schema_path = os.path.join(os.path.dirname(__file__), 'graphql_schema.sql')
+from pathlib import Path
+schema_path = Path(__file__).parent / 'graphql_schema.sql'
 
 PATCH = """
 ALTER TABLE "graphql"."user" ALTER COLUMN credentials_id DROP NOT NULL;
 """
 
 try:
-    with open(schema_path) as f:
+    with schema_path.open('r') as f:
         GRAPHQL_SQL_SCHEMA = f.read() + PATCH
 except IOError:
     raise AssertionError('graphql_schema.sql is missing. Run `yarn run gen-sql-schema` in the graphql project.')
