b686bd7a80f047581fcf22d5cf1a8623d1ea8c8d
Ren22 <ngm.renat@gmail.com>
2019-03-06 17:46:02 +0100

    Added 'h1', 'h2', 'del' to allowed tags

--- metaspace/webapp/src/util.ts
+++ metaspace/webapp/src/util.ts
@@ -114,9 +114,9 @@ function sanitizeIt(descriptionText: string) {
   return sanitizeHtml(
     descriptionText,
     {
-      allowedTags: [ 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+      allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
         'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-        'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'],
+        'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'del'],
       allowedAttributes: {
         'a': ['href', 'rel']
       },
