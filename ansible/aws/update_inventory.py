4dc4ba7e5aebd55c950506da14d7d72df1d5a2dc
Vitaly Kovalev <kovalev@embl.de>
2019-06-14 20:58:06 +0200

    Ansible configs transfer fixes (#343)
    * fix update_inventory script
    
    * boto default credentials file support
    
    * update paths to ansible config file in scripts
    
    * update ansible template files and readme
    
    * remove vault password config var from ansible.cfg when deploying sm_cluster_autostart_app
    
    * provide --credentials-file argument to update_inventory script

--- ansible/aws/update_inventory.py
+++ ansible/aws/update_inventory.py
@@ -10,8 +10,10 @@ from os import path
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ansible inventory file updater')
     parser.add_argument('--stage', dest='stage', default='dev', type=str, help='One of dev/stage/prod')
+    parser.add_argument('--credentials-file', dest='cred_file', action='store_true',
+                        help='Use AWS credentials file')
     args = parser.parse_args()
-    ansible_config_path = path.join(args.stage, 'group_vars/all.yml')
+    ansible_config_path = path.join(args.stage, 'group_vars/all/vars.yml')
     ansible_config = yaml.load(open(ansible_config_path))
     inv_file = path.join(args.stage, 'hosts')
 
@@ -20,8 +22,12 @@ if __name__ == '__main__':
     inventory = ConfigParser(allow_no_value=True)
     inventory.read_file(open(inv_file))
 
-    session = boto3.session.Session(aws_access_key_id=ansible_config['aws_access_key_id'],
-                                    aws_secret_access_key=ansible_config['aws_secret_access_key'])
+    if args.cred_file:  # use default credentials file
+        session = boto3.session.Session()
+    else:
+        session = boto3.session.Session(aws_access_key_id=ansible_config['aws_access_key_id'],
+                                        aws_secret_access_key=ansible_config['aws_secret_access_key'])
+
     ec2 = session.resource('ec2', ansible_config['aws_region'])
 
     for component, spec in ansible_config['cluster_configuration']['instances'].items():
