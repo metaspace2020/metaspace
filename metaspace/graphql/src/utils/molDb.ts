2e78af60098d8ee6e6e3a86e02ecec188357ccd6
Vitaly Kovalev <kovalev@embl.de>
2019-06-25 19:55:09 +0200

    hide M4I databases from most users

--- metaspace/graphql/src/utils/molDb.ts
+++ metaspace/graphql/src/utils/molDb.ts
@@ -3,8 +3,8 @@ import config from './config';
 
 export const deprecatedMolDBs = new Set([
   'HMDB', 'ChEBI', 'LIPID_MAPS', 'SwissLipids', 'COTTON_HMDB', 'HMDB-v2.5', 'HMDB-v2.5-cotton',
-  // EMBL-dev1/2 aren't actually deprecated. Their inclusion in this list is just for hiding them from normal users in the UI
-  'EMBL-dev1', 'EMBL-dev2',
+  // EMBL-dev1/2, M4I_* aren't actually deprecated. Their inclusion in this list is just for hiding them from normal users in the UI
+  'EMBL-dev1', 'EMBL-dev2', 'M4I_1',
 ]);
 
 export async function fetchMolecularDatabases() {
