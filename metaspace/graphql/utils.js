36b8cb632a9c35bc90c1d2a4a3b4275bace03bc3
Vitaly Kovalev <kovalev@embl.de>
2019-06-25 20:07:28 +0200

    move deprecatedMolDBs to right module

--- metaspace/graphql/utils.js
+++ metaspace/graphql/utils.js
@@ -52,8 +52,8 @@ const logger = new (winston.Logger)({
 
 const deprecatedMolDBs = new Set([
   'HMDB', 'ChEBI', 'LIPID_MAPS', 'SwissLipids', 'COTTON_HMDB', 'HMDB-v2.5', 'HMDB-v2.5-cotton',
-  // EMBL-dev1/2 aren't actually deprecated. Their inclusion in this list is just for hiding them from normal users in the UI
-  'EMBL-dev1', 'EMBL-dev2',
+  // EMBL-dev1/2, M4I_* aren't actually deprecated. Their inclusion in this list is just for hiding them from normal users in the UI
+  'EMBL-dev1', 'EMBL-dev2', 'M4I_1',
 ]);
 
 async function fetchMolecularDatabases() {
