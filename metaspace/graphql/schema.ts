4de682cb69f230a9f1e605e81d2a0ac1f45708e4
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2019-08-12 14:30:00 +0200

    Update generate-json-graphql-schema.js to fix CircleCI tests

--- metaspace/graphql/schema.ts
+++ metaspace/graphql/schema.ts
@@ -3,6 +3,7 @@ import {mergeSchemas, makeExecutableSchema} from 'graphql-tools';
 import * as fs from 'fs';
 
 export const mergedSchemas = mergeTypes([
+  // NOTE: This list should be kept in sync with bin/generate-json-graphql-schema.js
   fs.readFileSync('schemas/lookups.graphql', 'utf8'),
   fs.readFileSync('schemas/annotation.graphql', 'utf8'),
   fs.readFileSync('schemas/dataset.graphql', 'utf8'),
