ef9e7103617b39cf7b35d7893fa8f7f629312741
Vitaly Kovalev <kovalev@embl.de>
2019-08-30 17:54:08 +0200

    add migration to update molecular database names (#407)

---
+++ metaspace/engine/migrations/r1_5_20190829_update_moldb_names.py
@@ -0,0 +1,32 @@
+import argparse
+import logging
+
+import pandas as pd
+
+from sm.engine.db import DB
+from sm.engine.util import init_loggers
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Update molecular database molecule names')
+    parser.add_argument('--config', default='conf/config.json', help='SM config path')
+    parser.add_argument('file_path', help='Path to file with new names')
+    args = parser.parse_args()
+    init_loggers()
+    logger = logging.getLogger('engine')
+
+    logger.info(f'Importing new names from {args.file_path}')
+
+    db_config = {
+        "host": "localhost",
+        "database": "mol_db",
+        "user": "mol_db"
+    }
+    db = DB(db_config)
+    names_df = pd.read_csv(args.file_path, sep='\t')[['id', 'name']]
+
+    sql = (
+        'WITH molecule_name AS (SELECT UNNEST(%s::text[]) as id_, UNNEST(%s::text[]) as name_) '
+        'UPDATE molecule SET mol_name = molecule_name.name_ '
+        'FROM molecule_name WHERE molecule.mol_id = molecule_name.id_'
+    )
+    db.alter(sql, [names_df.id.values.tolist(), names_df.name.values.tolist()])
