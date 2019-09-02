8ec9b92b51ba4151b95b66d709cc60ba74785711
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2019-09-02 17:29:23 +0200

    Refactor `resolvers.js` into TypeScript Add resolvers: chemModSuggestions, neutralLossSuggestions

---
+++ metaspace/graphql/src/modules/annotation/controller/Query.ts
@@ -0,0 +1,28 @@
+import {applyQueryFilters} from '../queryFilters';
+import {esAnnotationByID, esCountResults, esSearchResults} from '../../../../esConnector';
+import {FieldResolversFor} from '../../../bindingTypes';
+import {Query} from '../../../binding';
+
+const QueryResolvers: FieldResolversFor<Query, void> = {
+  async allAnnotations(source, args, ctx) {
+    const {postprocess, args: newArgs} = await applyQueryFilters(ctx, args);
+    let annotations = await esSearchResults(newArgs, 'annotation', ctx.user);
+
+    if (postprocess != null) {
+      annotations = postprocess(annotations);
+    }
+
+    return annotations;
+  },
+
+  async countAnnotations(source, args, ctx) {
+    const {args: newArgs} = await applyQueryFilters(ctx, args);
+
+    return await esCountResults(newArgs, 'annotation', ctx.user);
+  },
+
+  async annotation(_, { id }, ctx) {
+    return await esAnnotationByID(id, ctx.user);
+  },
+};
+export default QueryResolvers;
