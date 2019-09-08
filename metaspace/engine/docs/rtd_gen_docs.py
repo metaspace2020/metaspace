e02244bb2549809c338555b019de138c2fe17a1c
Vitaly Kovalev <kovalev@embl.de>
2019-09-08 14:46:20 +0200

    format the rest of all code in engine dir

--- metaspace/engine/docs/rtd_gen_docs.py
+++ metaspace/engine/docs/rtd_gen_docs.py
@@ -7,7 +7,13 @@ rootdir = dirname(curdir)
 conf_dir = join(rootdir, 'conf')
 apidoc_cmd = 'sphinx-apidoc'
 apidoc_exclusions = ' '.join(
-    [join(rootdir, 'pyMSpec'), join(rootdir, 'pyImagingMSpec'), join(rootdir, 'tests'), join(rootdir, 'engine', 'tests')])
+    [
+        join(rootdir, 'pyMSpec'),
+        join(rootdir, 'pyImagingMSpec'),
+        join(rootdir, 'tests'),
+        join(rootdir, 'engine', 'tests'),
+    ]
+)
 apidoc_params = '-f -o {} {} {}'.format(curdir, rootdir, apidoc_exclusions)
 
 
