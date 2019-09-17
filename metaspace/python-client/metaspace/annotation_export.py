aa36cf2d51a4d1d9c1d4b211b2939729367568f9
Vitaly Kovalev <kovalev@embl.de>
2019-09-17 14:28:41 +0200

    make metaspace/annotation_export script so it can be used directly with python -m metaspace.annotation_export

--- metaspace/python-client/metaspace/annotation_export.py
+++ metaspace/python-client/metaspace/annotation_export.py
@@ -216,3 +216,26 @@ def export_molecules(ann_stat_df, mol_df, path):
         mol_export_df.id.to_csv(export_path / 'molecules.csv', index=False, header=False)
     else:
         logger.info(f'Annotation or molecule dataframe is empty. Nothing to export')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run molecule export')
+    parser.add_argument('--path', default='.', help='Export path')
+    parser.add_argument('--url', type=str, help='Metaspace URL, in double quotes')
+    parser.add_argument(
+        '--coloc-thr', type=float, default=None, help='Colocalization coeff threshold'
+    )
+    args = parser.parse_args()
+
+    url = args.url
+    path = Path(args.path)
+    coloc_thr = args.coloc_thr
+
+    init_logger(logging.DEBUG)
+
+    filter_args = convert_url_to_filter_args(url)
+    graphql_res = fetch_graphql_res(filter_args)
+    ann_df, mol_df = convert_to_dfs(graphql_res)
+    ann_df = filter_ann_df(ann_df, coloc_thr)
+    ann_stat_df = calculate_ann_stat(ann_df)
+    export_molecules(ann_stat_df, mol_df, path)
