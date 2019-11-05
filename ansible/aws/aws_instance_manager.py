3cb8594f9e67dda068acb5d07fa7ecc4ce4b00c3
Vitaly Kovalev <kovalev@embl.de>
2019-11-05 17:19:53 +0100

    fix stop_all_instances method when price is not specified

--- ansible/aws/aws_instance_manager.py
+++ ansible/aws/aws_instance_manager.py
@@ -260,7 +260,7 @@ class AWSInstManager:
     def stop_all_instances(self, components):
         for component in components:
             i = self.conf['instances'][component]
-            method = 'stop' if i['price'] is None else 'terminate'
+            method = 'stop' if not i.get('price', None) else 'terminate'
             alarms = [self.conf['alarms'][id] for id in i.get('alarms', [])]
             self.stop_instances(i['hostgroup'], method=method, alarms=alarms)
 
