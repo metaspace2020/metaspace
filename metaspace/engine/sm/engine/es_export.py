Update isobar changes to use correct ppm scaling for analysis_version_2

metaspace/engine/sm/engine/es_export.py
@@ -401,7 +401,7 @@ class ESExporter:
             annotation_counts[fdr] += 1
 
         self._add_isomer_fields_to_anns(annotation_docs)
-        ESExporterIsobars.add_isobar_fields_to_anns(annotation_docs, ds_doc)
+        ESExporterIsobars.add_isobar_fields_to_anns(annotation_docs, isocalc)
         to_index = []
         for doc in annotation_docs:
             to_index.append(
@@ -564,15 +564,15 @@ class ESExporterIsobars:
     """
 
     @classmethod
-    def add_isobar_fields_to_anns(cls, ann_docs, ds_doc):
-        mzs_df = cls._build_mzs_df(ann_docs, ds_doc)
+    def add_isobar_fields_to_anns(cls, ann_docs, isocalc):
+        mzs_df = cls._build_mzs_df(ann_docs, isocalc)
 
         for _, peak_rows in mzs_df.groupby('id'):
             overlaps = cls._find_overlaps(mzs_df, peak_rows)
             cls._apply_overlap_group(peak_rows, overlaps)
 
     @staticmethod
-    def _build_mzs_df(ann_docs, ds_doc):
+    def _build_mzs_df(ann_docs, isocalc):
         peaks = []
 
         for doc in ann_docs:
@@ -597,10 +597,7 @@ class ESExporterIsobars:
         mzs_df = peaks_df.sort_values('mz')
 
         # After feature/analysis_version_2 is merged, use this code instead:
-        # mzs_df['lower_mz'], mzs_df['upper_mz'] = isocalc.mass_accuracy_bounds(mzs_df['mz'])
-        ppm = ds_doc['ds_config']['image_generation']['ppm']
-        mzs_df['lower_mz'] = mzs_df['mz'] - mzs_df['mz'] * ppm * 1e-6
-        mzs_df['upper_mz'] = mzs_df['mz'] + mzs_df['mz'] * ppm * 1e-6
+        mzs_df['lower_mz'], mzs_df['upper_mz'] = isocalc.mass_accuracy_bounds(mzs_df['mz'])
         mzs_df['lower_idx'] = np.searchsorted(mzs_df.upper_mz.values, mzs_df.lower_mz.values, 'l')
         mzs_df['upper_idx'] = np.searchsorted(mzs_df.lower_mz.values, mzs_df.upper_mz.values, 'r')
         return mzs_df