c5bbca972e4405fb432388bf52dab43274c3f2f2
Richard Goater <richard.goater27@gmail.com>
2020-02-18 15:34:59 +0100

    wip

--- metaspace/webapp/src/modules/Project/ReviewLink.tsx
+++ metaspace/webapp/src/modules/Project/ReviewLink.tsx
@@ -1,3 +1,5 @@
+import './review.css'
+
 // import Vue from 'vue'
 import { createComponent, computed } from '@vue/composition-api'
 import { Button } from 'element-ui'
@@ -19,30 +21,42 @@ const ReviewLink = createComponent({
       return `${window.location.origin}/api_auth/review?prj=${props.projectId}&token=${props.reviewToken}`
     })
     return () => (
-      <section>
-        <h2>Review Link</h2>
-        <p>
-          {props.publicationStatus}
-        </p>
-        <div>
+      <ol class="sm-workflow sm-hipster-style">
+        <li class="done">
+          <h2>Create a review link</h2>
+          <p>
+            A review link allows reviewers to access this project and its datasets without making the project public.
+          </p>
+        </li>
+        <li>
+          <h2>Review in progress</h2>
+          <p>Reviewers can access this project using the following link:</p>
           {reviewLink.value
             && <p>
               <a href={reviewLink.value}>{reviewLink.value}</a>
             </p>}
-          {props.publicationStatus === 'UNPUBLISHED'
-            && <Button onClick={props.createLink}>
-              Create Link
-            </Button>}
-          {props.publicationStatus === 'UNDER_REVIEW'
-            && <Button onClick={props.deleteLink} type="danger">
-                Delete Link
-            </Button>}
-          {props.publicationStatus === 'UNDER_REVIEW'
-            && <Button onClick={props.publishProject} type="success">
-              Publish Project
-            </Button>}
-        </div>
-      </section>
+        </li>
+        <li></li>
+      </ol>
+      // <div>
+      //   <h2>{props.publicationStatus}</h2>
+      //   {reviewLink.value && props.publicationStatus === 'UNDER_REVIEW'
+      //   && <p>
+      //     <a href={reviewLink.value}>{reviewLink.value}</a>
+      //   </p>}
+      //   {props.publicationStatus === 'UNPUBLISHED'
+      //   && <Button onClick={props.createLink}>
+      //     Create Link
+      //   </Button>}
+      //   {props.publicationStatus === 'UNDER_REVIEW'
+      //   && <Button onClick={props.deleteLink} type="danger">
+      //       Delete Link
+      //   </Button>}
+      //   {props.publicationStatus === 'UNDER_REVIEW'
+      //   && <Button onClick={props.publishProject} type="success">
+      //     Publish Project
+      //   </Button>}
+      // </div>
     )
   },
 })
