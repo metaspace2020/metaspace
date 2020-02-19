16e0eee12c1a88f1c942bff792c5ecce38e79260
Richard Goater <richard.goater27@gmail.com>
2020-02-19 17:26:06 +0100

    wip section transitions working

--- metaspace/webapp/src/modules/Project/Review.tsx
+++ metaspace/webapp/src/modules/Project/Review.tsx
@@ -3,6 +3,28 @@ import './review.css'
 // import Vue from 'vue'
 import { createComponent, computed } from '@vue/composition-api'
 import { Button } from 'element-ui'
+// import classnames from 'classnames'
+
+const statuses = {
+  UNPUBLISHED: 'UNPUBLISHED',
+  UNDER_REVIEW: 'UNDER_REVIEW',
+  PUBLISHED: 'PUBLISHED',
+}
+
+const WorkflowItem = createComponent({
+  props: {
+    active: Boolean,
+    done: Boolean,
+  },
+  setup(props, { slots }) {
+    console.log(props, slots)
+    return () => (
+      <li class={{ active: props.active, done: props.done }}>
+        {slots.default()}
+      </li>
+    )
+  },
+})
 
 const ReviewLink = createComponent({
   props: {
@@ -22,20 +44,41 @@ const ReviewLink = createComponent({
     })
     return () => (
       <ol class="sm-workflow">
-        <li class="done">
+        <WorkflowItem
+          active={props.publicationStatus === statuses.UNPUBLISHED}
+          done={
+            props.publicationStatus === statuses.UNDER_REVIEW
+            || props.publicationStatus === statuses.PUBLISHED
+          }
+        >
           <h2 class="sm-workflow-header">Create a review link</h2>
           <p>
             A <b>review link</b> allows reviewers to access this project and its datasets without making the project public.
           </p>
-        </li>
-        <li class="active">
+          <form>
+            {props.publicationStatus === statuses.UNPUBLISHED
+              && <Button onClick={props.createLink} type="primary">
+                Create Link
+              </Button>
+            }
+            {props.publicationStatus === statuses.UNDER_REVIEW
+              && <Button onClick={props.deleteLink}>
+                Remove Link
+              </Button>
+            }
+          </form>
+        </WorkflowItem>
+        <WorkflowItem
+          active={props.publicationStatus === statuses.UNDER_REVIEW}
+          done={props.publicationStatus === statuses.PUBLISHED}
+        >
           <h2 class="sm-workflow-header">Review in progress</h2>
           <p>Reviewers can access this project using the following link:</p>
           {reviewLink.value
             && <p>
               <a href={reviewLink.value}>{reviewLink.value}</a>
             </p>}
-        </li>
+        </WorkflowItem>
         <li>
           <h2 class="sm-workflow-header">Publish results</h2>
           <p>This project and its datasets are now public.</p>
@@ -65,4 +108,3 @@ const ReviewLink = createComponent({
 })
 
 export default ReviewLink
-
