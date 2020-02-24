214b5a6b89f88bde0ef8937de8b84eccb3d69904
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2020-02-24 13:50:07 +0100

    Fix unscoped css class

--- metaspace/webapp/src/components/IonImageViewer.vue
+++ metaspace/webapp/src/components/IonImageViewer.vue
@@ -1,2 +1,8 @@
 <script lang="ts" src="./IonImageViewer.tsx" />
 <style src="./IonImageViewer.scss" lang="scss" scoped />
+<style>
+  /* Unscoped, because the tooltip is appended to document.body */
+  .pointer-events-none {
+    pointer-events: none;
+  }
+</style>
