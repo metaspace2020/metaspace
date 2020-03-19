46e38e32ccc871f81358b12ee0b4d0d021f4e7fd
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2020-03-19 23:43:27 +0100

    build: pin postgres version

--- docker/postgres/initdb.d/create-sm.sh
+++ docker/postgres/initdb.d/create-sm.sh
@@ -1,4 +1,2 @@
-psql "$POSTGRES_DB" "$POSTGRES_USER" -c "CREATE DATABASE sm OWNER sm;"
-psql sm "$POSTGRES_USER" -c "CREATE EXTENSION \"uuid-ossp\";"
 psql sm "$POSTGRES_USER" -c "CREATE SCHEMA graphql;"
 psql sm "$POSTGRES_USER" -c "ALTER SCHEMA graphql OWNER TO sm;"
