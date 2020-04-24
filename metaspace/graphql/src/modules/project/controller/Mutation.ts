646ff2a7199405154964488b04ed10d21774dc59
Richard Goater <richard.goater27@gmail.com>
2020-04-24 13:07:01 +0100

    Revert "fix(graphql): bug that causes all datasets to become public on project publish"
    This reverts commit 382c1ff4cb778189cd222c54e5940a6d71bea53c.

--- metaspace/graphql/src/modules/project/controller/Mutation.ts
+++ metaspace/graphql/src/modules/project/controller/Mutation.ts
@@ -264,9 +264,11 @@ const MutationResolvers: FieldResolversFor<Mutation, void> = {
       isPublic: true
     });
 
-    const affectedDatasets = await ctx.entityManager.find(DatasetProjectModel, { where: { projectId } });
-    await Promise.all(affectedDatasets.map(async dp => {
-      await smAPIUpdateDataset(dp.datasetId, { isPublic: true });
+    const affectedDatasets = await ctx.entityManager.find(DatasetModel, {
+      where: { datasetProjects: { projectId } },
+    });
+    await Promise.all(affectedDatasets.map(async ds => {
+      await smAPIUpdateDataset(ds.id, { isPublic: true });
     }));
 
     return await ctx.entityManager.getCustomRepository(ProjectSourceRepository)
@@ -285,9 +287,11 @@ const MutationResolvers: FieldResolversFor<Mutation, void> = {
     });
 
     if (isPublic != null) {
-      const affectedDatasets = await ctx.entityManager.find(DatasetProjectModel, { where: { projectId } });
-      await Promise.all(affectedDatasets.map(async dp => {
-        await smAPIUpdateDataset(dp.datasetId, { isPublic });
+      const affectedDatasets = await ctx.entityManager.find(DatasetModel, {
+        where: { datasetProjects: { projectId } },
+      });
+      await Promise.all(affectedDatasets.map(async ds => {
+        await smAPIUpdateDataset(ds.id, { isPublic });
       }));
     }
 
