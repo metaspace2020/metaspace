10bdf79c8f4a32c8d9e98c8f8b199bf4a03da265
Richard Goater <richard.goater27@gmail.com>
2020-04-24 12:29:01 +0100

    Attempt to fix server-side date handling

--- metaspace/graphql/src/utils/typeOrmConfig.ts
+++ metaspace/graphql/src/utils/typeOrmConfig.ts
@@ -1,14 +1,22 @@
-import {ConnectionOptions} from 'typeorm';
+import { ConnectionOptions } from 'typeorm';
 import config from './config';
-import {AUTH_ENTITIES} from '../modules/auth/model';
-import {USER_ENTITIES} from '../modules/user/model';
-import {DATASET_ENTITIES} from '../modules/dataset/model';
-import {GROUP_ENTITIES} from '../modules/group/model';
-import {PROJECT_ENTITIES} from '../modules/project/model';
-import {ANNOTATION_ENTITIES} from '../modules/annotation/model';
-import {ENGINE_ENTITIES} from '../modules/engine/model';
-import {MOLECULAR_DB_ENTITIES} from '../modules/moldb/model';
-import {SnakeCaseNamingStrategy} from './SnakeCaseNamingStrategy';
+import { AUTH_ENTITIES } from '../modules/auth/model';
+import { USER_ENTITIES } from '../modules/user/model';
+import { DATASET_ENTITIES } from '../modules/dataset/model';
+import { GROUP_ENTITIES } from '../modules/group/model';
+import { PROJECT_ENTITIES } from '../modules/project/model';
+import { ANNOTATION_ENTITIES } from '../modules/annotation/model';
+import { ENGINE_ENTITIES } from '../modules/engine/model';
+import { MOLECULAR_DB_ENTITIES } from '../modules/moldb/model';
+import { SnakeCaseNamingStrategy } from './SnakeCaseNamingStrategy';
+import * as pg from 'pg'
+
+// https://github.com/typeorm/typeorm/issues/4519#issuecomment-606319943
+// all `Date`s should be assumed to be in UTC when being sent to Postgres
+pg.defaults.parseInputDatesAsUTC = true;
+// all dates received from Postgres should be assumed to be UTC when converted to `Date`s
+const TIMESTAMP = '1114' // https://github.com/brianc/node-pg-types/blob/master/lib/builtins.js#L47
+pg.types.setTypeParser(TIMESTAMP, (stringValue: string) => new Date(`${stringValue}Z`));
 
 export const DbSchemaName = 'graphql';
 
