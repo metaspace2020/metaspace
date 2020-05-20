6ff036d309f444049aaa3b6ed90f1eb9b13b0060
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2020-05-20 20:33:44 +0200

    fix(graphql): don't migrate proj desc that are already json

--- metaspace/graphql/src/migrations/1589903035439-ProjectDescription.ts
+++ metaspace/graphql/src/migrations/1589903035439-ProjectDescription.ts
@@ -7,10 +7,11 @@ export class ProjectDescription1589903035439 implements MigrationInterface {
         await queryRunner.query(`
             UPDATE graphql.project
             SET project_description = jsonb_set(
-                '{"type": "doc","content": [{"type":"paragraph", "content": [{"type": "text", "text": ""}]}]}'::jsonb,
-                '{content,0,content,0,text}',
-                to_jsonb(project_description))::json
-            WHERE project_description IS NOT NULL;
+                '{"type": "doc", "content": [{"type": "paragraph", "content": [{"type": "text", "text": ""}]}]}'::jsonb,
+                '{content, 0, content, 0, text}',
+                to_jsonb(project_description)
+            )::json
+            WHERE project_description IS NOT NULL AND project_description NOT LIKE '{%}';
         `);
     }
 
