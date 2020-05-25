5f064629e9cb0e2a64c7ad919ceca1c382b99503
Richard Goater <richard.goater27@gmail.com>
2020-05-25 14:15:22 +0100

    Remove unneeded things [skip ci]

--- metaspace/webapp/src/modules/App/cookies.js
+++ metaspace/webapp/src/modules/App/cookies.js
@@ -1,10 +1,8 @@
 // https://github.com/dobarkod/cookie-banner/blob/4801ba44199009f8a2af88653d6e728057cd108b/src/cookiebanner.js#L79
 const Cookies = {
-  // @ts-ignore
   get: function (key) {
     return decodeURIComponent(document.cookie.replace(new RegExp('(?:(?:^|.*;)\\s*' + encodeURIComponent(key).replace(/[-.+*]/g, '\\$&') + '\\s*\\=\\s*([^;]*).*$)|^.*$'), '$1')) || null
   },
-  // @ts-ignore
   set: function ({ key, val = 1, end = Infinity, path = '/', domain = null, secure = false }) {
     if (!key || /^(?:expires|max-age|path|domain|secure)$/i.test(key)) {
       return false
@@ -26,11 +24,9 @@ const Cookies = {
     document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(val) + expires + (domain ? '; domain=' + domain : '') + (path ? '; path=' + path : '') + (secure ? '; secure' : '')
     return true
   },
-  // @ts-ignore
   has: function (key) {
     return (new RegExp('(?:^|;\\s*)' + encodeURIComponent(key).replace(/[-.+*]/g, '\\$&') + '\\s*\\=')).test(document.cookie)
   },
-  // @ts-ignore
   remove: function (key, path, domain) {
     if (!key || !this.has(key)) { return false }
     document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT' + (domain ? '; domain=' + domain : '') + (path ? '; path=' + path : '')
