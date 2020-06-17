3e5837db09216988d289aafca6b2621929a63b21
Richard Goater <richard.goater27@gmail.com>
2020-06-17 19:49:05 +0200

    (docker) add script to reindex ES locally

---
+++ docker/reindex-es.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Example usage
+# ~~~~~~~~~~~~~
+# ```
+# ./reindex-es.sh
+# ./reindex-es.sh --ds-id <id>
+# ```
+
+args=${@:-'--ds-name "%"'}
+
+docker-compose exec sm-api \
+       	bash -c \
+	"cd /opt/dev/metaspace/metaspace/engine && python -m scripts.update_es_index $args"
