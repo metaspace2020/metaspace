00f33bdd43856084d9d87e42defefa8c5e010b37
Richard Goater <richard.goater27@gmail.com>
2020-07-07 12:43:45 +0200

    Fix field names [skip CI]

--- metaspace/webapp/src/api/moldb.ts
+++ metaspace/webapp/src/api/moldb.ts
@@ -27,7 +27,7 @@ export const databaseListItemsQuery =
       id
       name
       version
-      public
+      isPublic
       archived
     }
   }`
@@ -41,7 +41,7 @@ export const databaseDetailsQuery =
       fullName
       link
       name
-      public
+      isPublic
       version
     }
   }`
