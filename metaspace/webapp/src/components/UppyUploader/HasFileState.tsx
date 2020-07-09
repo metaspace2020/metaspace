8464b44ed0028adfa65b588bd70d604c484a2d33
Richard Goater <richard.goater27@gmail.com>
2020-07-09 17:05:53 +0200

    Make sure transition only receives a single child node

--- metaspace/webapp/src/components/UppyUploader/HasFileState.tsx
+++ metaspace/webapp/src/components/UppyUploader/HasFileState.tsx
@@ -1,13 +1,9 @@
-import { createComponent, reactive } from '@vue/composition-api'
+import { createComponent } from '@vue/composition-api'
 
 import '../../components/ColourIcon.css'
 import FileIcon from '../../assets/inline/refactoring-ui/document.svg'
 import FadeTransition from '../../components/FadeTransition'
 import ProgressRing from '../../components/ProgressRing'
-
-interface State {
-  progress: number
-}
 
 interface Props {
   status: 'UPLOADING' | 'COMPLETE' | 'ERROR' | 'DISABLED'
@@ -24,54 +20,65 @@ export default createComponent<Props>({
     buttonClickHandler: Function,
   },
   setup(props) {
-    return () => (
-      <div class={['text-sm leading-5 transition-opacity duration-300', props.status === 'DISABLED' && 'opacity-50']}>
-        <div class="relative mt-3">
-          <FileIcon class="sm-colour-icon sm-colour-icon--large" />
-          <ProgressRing
-            class={[
-              'absolute top-0 left-0',
-              {
-                'text-success': props.status === 'COMPLETE',
-                'text-primary': props.status === 'UPLOADING',
-                'text-danger': props.status === 'ERROR',
-              },
-            ]}
-            radius={24}
-            stroke={4}
-            progress={props.progress}
-          />
-          <FadeTransition class="absolute top-0 right-0 -mt-3 -mr-6">
-            { props.status === 'UPLOADING'
-              && <span>{props.progress}%</span> }
-            { props.status === 'COMPLETE'
-              && <button
-                class="button-reset text-gray-600 hover:text-primary focus:text-primary"
-                title="Remove file"
-                onClick={props.buttonClickHandler}
-              >
-                <i class="el-icon-error text-inherit text-lg"></i>
-              </button> }
+    return () => {
+      let fileAction
+
+      switch (props.status) {
+        case 'UPLOADING':
+          fileAction = <span>{props.progress}%</span>
+          break
+        case 'COMPLETE':
+        case 'ERROR':
+          fileAction = (
+            <button
+              key={props.status}
+              class="button-reset text-gray-600 hover:text-primary focus:text-primary"
+              title={props.status === 'COMPLETE' ? 'Remove file' : 'Retry file'}
+              onClick={props.buttonClickHandler}
+            >
+              <i
+                class={[
+                  'text-inherit text-lg',
+                  props.status === 'COMPLETE' ? 'el-icon-error' : 'el-icon-refresh',
+                ]}
+              />
+            </button>
+          )
+          break
+      }
+
+      return (
+        <div class={['text-sm leading-5 transition-opacity duration-300', props.status === 'DISABLED' && 'opacity-50']}>
+          <div class="relative mt-3">
+            <FileIcon class="sm-colour-icon sm-colour-icon--large" />
+            <ProgressRing
+              class={[
+                'absolute top-0 left-0',
+                {
+                  'text-success': props.status === 'COMPLETE',
+                  'text-primary': props.status === 'UPLOADING',
+                  'text-danger': props.status === 'ERROR',
+                },
+              ]}
+              radius={24}
+              stroke={4}
+              progress={props.progress}
+            />
+            <FadeTransition class="absolute top-0 right-0 -mt-3 -mr-6">
+              {fileAction}
+            </FadeTransition>
+          </div>
+          <FadeTransition>
             { props.status === 'ERROR'
-              && <button
-                class="button-reset text-gray-600 hover:text-primary focus:text-primary"
-                title="Retry file"
-                onClick={props.buttonClickHandler}
-              >
-                <i class="el-icon-refresh text-inherit text-lg"></i>
-              </button> }
+              ? <p key="error" class="m-0 mt-3 font-medium text-danger">
+                Upload failed
+              </p>
+              : <p class="m-0 mt-3 font-medium">
+                {props.fileName}
+              </p> }
           </FadeTransition>
         </div>
-        <FadeTransition>
-          { props.status === 'ERROR'
-            ? <p key="error" class="m-0 mt-3 font-medium text-danger">
-              Upload failed
-            </p>
-            : <p class="m-0 mt-3 font-medium">
-              {props.fileName}
-            </p> }
-        </FadeTransition>
-      </div>
-    )
+      )
+    }
   },
 })
