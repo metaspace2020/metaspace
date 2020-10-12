e52540fef452d497414980ce87c611d2dc9a3226
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2020-10-12 15:11:06 +0200

    Fix lint

--- metaspace/engine/sm/engine/sm_daemons.py
+++ metaspace/engine/sm/engine/sm_daemons.py
@@ -14,7 +14,7 @@ from sm.engine.colocalization import Colocalization
 from sm.engine.daemon_action import DaemonAction, DaemonActionStage
 from sm.engine.dataset import Dataset, DatasetStatus
 from sm.engine.db import DB
-from sm.engine.errors import AnnotationError, ImzMLError, IndexUpdateError, SMError, UnknownDSID
+from sm.engine.errors import AnnotationError, ImzMLError, IndexUpdateError, SMError
 from sm.engine.es_export import ESExporter
 from sm.engine.ion_thumbnail import generate_ion_thumbnail
 from sm.engine.isocalc_wrapper import IsocalcWrapper
