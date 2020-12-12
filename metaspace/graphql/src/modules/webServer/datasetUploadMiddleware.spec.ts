d1b83e4562fbbd3615935b88fabfc55fc7e9badb
Richard Goater <richard.goater27@gmail.com>
2020-12-12 01:45:43 +0000

    tests working maybe

--- metaspace/graphql/src/modules/webServer/datasetUploadMiddleware.spec.ts
+++ metaspace/graphql/src/modules/webServer/datasetUploadMiddleware.spec.ts
@@ -16,38 +16,43 @@ import * as supertest from 'supertest';
 import * as express from 'express';
 import datasetUploadMiddleware from './datasetUploadMiddleware';
 
+
+jest.mock('aws-sdk/clients/s3', () => {
+  return class MockS3 {
+    createMultipartUpload(options: any, cb: any) {
+      cb(null, { Key: options.Key })
+    }
+  }
+})
+
 describe.only('datasetUploadMiddleware', () => {
   let app: express.Express, uuid: string, uuidSignature: string
   beforeEach(async () => {
     app = express();
     app.use(datasetUploadMiddleware());
+
     const uuidResponse = await supertest(app)
       .get('/s3/uuid')
       .expect('Content-Type', /json/)
       .expect(200);
-
-    console.log(uuidResponse.body)
     uuid = uuidResponse.body.uuid;
     uuidSignature = uuidResponse.body.uuidSignature;
   });
 
   it('should return a key', async () => {
     const response = await supertest(app)
-      .post(`/s3/multipart?uuid=${uuid}&uuidSignature=${uuidSignature}`)
+      .post(`/s3/multipart?uuid=${uuid}&uuidSignature=${encodeURIComponent(uuidSignature)}`)
       .send({ filename: 'test.ibd', type: 'text/csv' })
       .expect('Content-Type', /json/)
       .expect(200);
-    console.log(response.body)
+
     expect(response.body).toMatchObject({ key: `${uuid}/test.ibd` });
   })
 
   it('should error on invalid signature', async () => {
-    const response = await supertest(app)
+    await supertest(app)
       .post(`/s3/multipart?uuid=${uuid}&uuidSignature=123`)
       .send({ filename: 'test.ibd', type: 'text/csv' })
-      .expect('Content-Type', /json/)
       .expect(500);
-
-    expect(response.body).toEqual({ invalid: true });
   })
 });
