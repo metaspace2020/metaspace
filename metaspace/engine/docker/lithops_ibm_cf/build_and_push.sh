82d7ae33b3a2e3471bef23f76416bad686e641b5
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2021-01-19 11:30:02 +0100

    Include dependencies in runtime for Lithops 2.2.15 support (but don't actually update to it yet)

--- metaspace/engine/docker/lithops_ibm_cf/build_and_push.sh
+++ metaspace/engine/docker/lithops_ibm_cf/build_and_push.sh
@@ -1,7 +1,7 @@
 if [[ $# -eq 0 ]] ; then
   echo "Builds and pushes Docker Hub"
-  echo "Usage: $0 image_name:tag"
-  echo "e.g. $0 metaspace2020/metaspace-lithops:1.8.0 pushes to the official repository at "
+  echo "Usage: $0 new_tag"
+  echo "e.g. $0 1.8.0 pushes to the official repository at "
   echo "https://hub.docker.com/repository/docker/metaspace2020/metaspace-lithops"
   exit 1
 fi
--- metaspace/engine/sm/engine/annotation_lithops/executor.py
+++ metaspace/engine/sm/engine/annotation_lithops/executor.py
@@ -22,7 +22,7 @@ TRet = TypeVar('TRet')
 #: manually updating their config files every time it changes. The image must be public on
 #: Docker Hub, and can be rebuilt using the scripts/Dockerfile in `engine/docker/lithops_ibm_cf`.
 #: Note: sci-test changes this constant to force local execution without docker
-RUNTIME_DOCKER_IMAGE = 'metaspace2020/metaspace-lithops:1.8.2'
+RUNTIME_DOCKER_IMAGE = 'metaspace2020/metaspace-lithops:1.8.3'
 MEM_LIMITS = {
     'ibm_cf': 4096,
     'ibm_vpc': 32768,
