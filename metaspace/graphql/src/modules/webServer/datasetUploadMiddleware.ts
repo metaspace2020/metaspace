bb097ba416681e9370dbf124228edef6ee98cce0
Richard Goater <richard.goater27@gmail.com>
2021-02-01 18:10:11 +0000

    Set higher limit for JSON body parsers in Uppy-related middleware

--- metaspace/graphql/src/modules/webServer/datasetUploadMiddleware.ts
+++ metaspace/graphql/src/modules/webServer/datasetUploadMiddleware.ts
@@ -64,7 +64,7 @@ export default function (httpServer?: http.Server) {
   };
 
   const router = Router()
-  router.use(bodyParser.json())
+  router.use(bodyParser.json({ limit: '1MB' }))
   router.get('/s3/uuid', generateUuidForUpload)
   router.use(companion.app(options))
   if (httpServer) {
