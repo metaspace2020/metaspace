0be85ece3709e5bdf20b5eb90365ace9628cc97f
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2021-02-06 13:02:13 +0100

    fix: use separate key/secret for storage in engine

--- metaspace/engine/sm/engine/storage.py
+++ metaspace/engine/sm/engine/storage.py
@@ -7,9 +7,9 @@ def get_boto3_s3_client():
     return boto3.client(
         's3',
         endpoint_url=config['storage']['endpoint_url'],
-        aws_access_key_id=config['aws']['aws_access_key_id'],
-        aws_secret_access_key=config['aws']['aws_secret_access_key'],
-        region_name=config['aws']['aws_region'],
+        aws_access_key_id=config['storage']['access_key_id'],
+        aws_secret_access_key=config['storage']['secret_access_key'],
+        region_name=config['aws']['aws_default_region'],
     )
 
 
@@ -18,8 +18,8 @@ def get_boto3_s3_bucket(bucket_name):
     return boto3.resource(
         's3',
         endpoint_url=config['storage']['endpoint_url'],
-        aws_access_key_id=config['aws']['aws_access_key_id'],
-        aws_secret_access_key=config['aws']['aws_secret_access_key'],
-        region_name=config['aws']['aws_region'],
+        aws_access_key_id=config['storage']['access_key_id'],
+        aws_secret_access_key=config['storage']['secret_access_key'],
+        region_name=config['aws']['aws_default_region'],
         config=boto3.session.Config(signature_version='s3v4'),
     ).Bucket(bucket_name)
