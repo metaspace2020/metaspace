8ae9c5f4c5413fb84529d3d16b3e6524fd987627
Richard Goater <richard.goater27@gmail.com>
2021-02-10 19:35:00 +0000

    lint

--- metaspace/engine/sm/engine/storage.py
+++ metaspace/engine/sm/engine/storage.py
@@ -1,6 +1,7 @@
 import boto3
 from sm.engine.config import SMConfig
 
+
 def _boto_client_kwargs():
     sm_config = SMConfig.get_conf()
     boto_config = boto3.session.Config(signature_version='s3v4')
@@ -11,13 +12,12 @@ def _boto_client_kwargs():
             aws_secret_access_key=sm_config['aws']['aws_secret_access_key'],
             config=boto_config,
         )
-    else:
-        return dict(
-            endpoint_url=sm_config['storage']['endpoint_url'],
-            aws_access_key_id=sm_config['storage']['access_key_id'],
-            aws_secret_access_key=sm_config['storage']['secret_access_key'],
-            config=boto_config,
-        )
+    return dict(
+        endpoint_url=sm_config['storage']['endpoint_url'],
+        aws_access_key_id=sm_config['storage']['access_key_id'],
+        aws_secret_access_key=sm_config['storage']['secret_access_key'],
+        config=boto_config,
+    )
 
 
 def get_s3_client():
