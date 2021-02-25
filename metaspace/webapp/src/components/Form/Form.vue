51726ff29da49a45c5954279609b6de9235b09cb
Richard Goater <richard.goater27@gmail.com>
2021-02-25 23:16:13 +0000

    Try to fix tests

--- metaspace/webapp/src/components/Form/Form.vue
+++ metaspace/webapp/src/components/Form/Form.vue
@@ -7,6 +7,12 @@
     <slot />
   </form>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  // for running in .jsx files
+})
+</script>
 <style scoped>
 form >>> .el-input {
   @apply py-1;
