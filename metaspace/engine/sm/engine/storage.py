148482d4ca684d1c0c3985394c7f9c8a422dfc08
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2021-03-15 17:56:20 +0100

    fix: create bucket, skip region for dev/ci env

--- metaspace/engine/sm/engine/storage.py
+++ metaspace/engine/sm/engine/storage.py
@@ -32,16 +32,14 @@ def get_s3_resource():
 
 def create_bucket(bucket_name: str):
     sm_config = SMConfig.get_conf()
+    region_name = sm_config['aws']['aws_default_region'] if 'aws' in sm_config else ''
     s3_client = get_s3_client()
     try:
         s3_client.head_bucket(Bucket=bucket_name)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == '404':
             s3_client.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={
-                    'LocationConstraint': sm_config['aws']['aws_default_region']
-                },
+                Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region_name},
             )
         else:
             raise
