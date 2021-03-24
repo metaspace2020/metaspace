152f609dfd0cd2a1391796717e88967132379521
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2021-03-24 10:18:52 -0300

    [AnnotationCOunTable] Added imports and added test scenarios

--- metaspace/webapp/src/modules/Datasets/overview/AnnotationCountTable.spec.ts
+++ metaspace/webapp/src/modules/Datasets/overview/AnnotationCountTable.spec.ts
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { sync } from 'vuex-router-sync'
 import { AnnotationCountTable } from './AnnotationCountTable'
+import { DatasetAnnotationCount } from '../../../api/dataset'
 
 Vue.use(Vuex)
 sync(store, router)
@@ -58,8 +59,10 @@ describe('AnnotationCountTable', () => {
       ],
     },
   ]
+  const id = 'xxxx'
   const mockFdrLevels = [5, 10, 20, 50]
-  const propsData = { data: mockAnnotationCounts, id: 'xxxx', header: mockFdrLevels }
+  const propsData = { data: mockAnnotationCounts, id: id, header: mockFdrLevels }
+  const propsEmptyData = { data: [], id: id, header: mockFdrLevels }
 
   const testHarness = Vue.extend({
     components: {
@@ -70,27 +73,27 @@ describe('AnnotationCountTable', () => {
     },
   })
 
-  it('should match snapshot', async() => {
+  it('it should match snapshot', async() => {
     const wrapper = mount(testHarness, { store, router, propsData })
     await Vue.nextTick()
 
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('should render the correct number of columns', async() => {
+  it('it should render the correct number of columns', async() => {
     const wrapper = mount(testHarness, { store, router, propsData })
     await Vue.nextTick()
     expect(wrapper.findAll('th').length).toBe(mockFdrLevels.length + 1)
   })
 
-  it('should render the correct link to navigate to annotation', async() => {
+  it('it should render the correct links to navigate to annotation', async() => {
     const wrapper = mount(testHarness, { store, router, propsData })
     await Vue.nextTick()
 
     // check if each link cell has the correct parameters
     wrapper.findAll('a').wrappers.forEach((link, index) => {
-      const mockIndex = Math.floor(index / (mockFdrLevels.length || 1))
-      const countIndex = index % mockFdrLevels.length
+      const mockIndex: number = Math.floor(index / (mockFdrLevels.length || 1))
+      const countIndex: number = index % mockFdrLevels.length
       expect(link.props('to').params.datasetId).toBe('xxxx')
       expect(link.props('to').query.db_id).toBe(mockAnnotationCounts[mockIndex].databaseId.toString())
       expect(link.props('to').query.fdr)
@@ -100,5 +103,51 @@ describe('AnnotationCountTable', () => {
     expect(wrapper.findAll('th').length).toBe(mockFdrLevels.length + 1)
     expect(wrapper.findAll('a').at(1).props('to').params.datasetId)
       .toBe('xxxx')
+  })
+
+  it('it should render with no data warning when props empty', async() => {
+    const wrapper = mount(testHarness, { store, router, propsData: propsEmptyData })
+    await Vue.nextTick()
+
+    expect(wrapper.find('.el-table__empty-text').exists()).toBeTruthy()
+  })
+
+  it('it should render the annotation count sum correctly', async() => {
+    const wrapper = mount(testHarness, { store, router, propsData })
+    await Vue.nextTick()
+
+    // check if each link cell has the correct parameters
+    wrapper.find('.el-table__footer-wrapper').findAll('td').wrappers.slice(1)
+      .forEach((footerItem, index) => {
+        const reducer = (accumulator: number, currentValue: DatasetAnnotationCount) => {
+          return accumulator + currentValue.counts[index].n
+        }
+        const sum = mockAnnotationCounts.reduce(reducer, 0)
+        expect(parseInt(footerItem.text(), 10)).toBe(sum)
+      })
+  })
+
+  it('it should render the correct databases name', async() => {
+    const wrapper = mount(testHarness, { store, router, propsData })
+    await Vue.nextTick()
+
+    // check if each link cell has the correct parameters
+    wrapper.find('.el-table__footer-wrapper').findAll('td').wrappers.slice(1)
+      .forEach((footerItem, index) => {
+        const reducer = (accumulator: number, currentValue: DatasetAnnotationCount) => {
+          return accumulator + currentValue.counts[index].n
+        }
+        const sum = mockAnnotationCounts.reduce(reducer, 0)
+        expect(parseInt(footerItem.text(), 10)).toBe(sum)
+      })
+  })
+
+  it('it should navigate to dataset annotation page on browse annotation button click', async() => {
+    const wrapper = mount(testHarness, { store, router, propsData })
+    await Vue.nextTick()
+
+    wrapper.find('button').trigger('click')
+    await Vue.nextTick()
+    expect(wrapper.vm.$route.path).toBe(`/dataset/${id}/annotations`)
   })
 })
