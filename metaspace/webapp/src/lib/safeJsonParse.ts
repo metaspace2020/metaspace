4e62328a3647adad375b959f50c65bac7c3e4738
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2021-03-25 16:43:44 -0300

    [Graphql|MetadataEditor|Datasetoverviewpage] added datasetDescription to dataset create and update|added datasetDesciption field|rendering datasetDescription improvement

--- metaspace/webapp/src/lib/safeJsonParse.ts
+++ metaspace/webapp/src/lib/safeJsonParse.ts
@@ -10,3 +10,15 @@ export default function safeJsonParse(json: string | null | undefined) {
   }
   return undefined
 }
+
+export const isValidTiptapJson = (doc: string) => {
+  try {
+    // The tiptap library can't validate JSON without having the full set of extensions configured, which seems like
+    // too much complexity to drag into graphql just for validation.
+    // It should be good enough to check that the field is valid JSON, and looks like it was created by tiptap.
+    const obj = JSON.parse(doc)
+    return obj.type === 'doc' && obj.content != null
+  } catch {
+    return false
+  }
+}
