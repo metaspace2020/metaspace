Add WIP EvalPeaksCollector Add LIPID MAPS DB Fix TOF mz scaling

metaspace/recal/msi_recal/math.py
@@ -1,4 +1,4 @@
-from typing import overload, Optional
+from typing import overload, Optional, List, Tuple
 
 import cpyMSpec
 import numpy as np
@@ -35,10 +35,7 @@ def mass_accuracy_bounds(mzs, analyzer: AnalyzerType, sigma_1: float):
     elif analyzer == 'orbitrap':
         half_width = mzs ** 1.5 * sigma_1
     else:
-        if np.isscalar(sigma_1):
-            half_width = sigma_1
-        else:
-            half_width = np.full_like(mzs, sigma_1)
+        half_width = mzs * sigma_1
 
     lower = mzs - half_width
     upper = mzs + half_width
@@ -104,7 +101,7 @@ def get_centroid_peaks(
     charge: int,
     min_abundance: float,
     instrument_model: cpyMSpec.InstrumentModel,
-):
+) -> List[Tuple[float, float]]:
     if adduct and adduct not in ('[M]+', '[M]-'):
         formula += adduct
     iso_pattern = cpyMSpec.isotopePattern(formula)
@@ -118,9 +115,12 @@ def get_centroid_peaks(
         # iso_pattern.centroids may raise an exception:
         #   Exception: b'the result contains no peaks, make min_abundance lower!'
         # If this happens, just return the most intense uncentroided theoretical peak.
-        if 'min_abundance' not in str(ex):
-            raise
-        return [(iso_pattern.masses[np.argmax(iso_pattern.intensities)], 1)]
+        if 'min_abundance' in str(ex):
+            return [(iso_pattern.masses[np.argmax(iso_pattern.intensities)], 1.0)]
+        if 'total number of' in str(ex) and 'less than zero' in str(ex):
+            # Invalid molecule due to adduct removing non-existent elements
+            raise Exception(f'Adduct could not be applied: {formula}{adduct}')
+        raise
 
 
 def get_mono_mz(formula: str, adduct: Optional[str], charge: int):
@@ -131,3 +131,13 @@ def get_mono_mz(formula: str, adduct: Optional[str], charge: int):
         iso_pattern.addCharge(charge)
 
     return iso_pattern.masses[np.argmax(iso_pattern.intensities)]
+
+
+def is_valid_formula_adduct(formula: str, adduct: str):
+    if adduct and adduct not in ('[M]+', '[M]-'):
+        formula += adduct
+    try:
+        cpyMSpec.isotopePattern(formula)
+        return True
+    except Exception:
+        return False

metaspace/recal/msi_recal/mean_spectrum.py
@@ -27,11 +27,6 @@ def _get_mean_spectrum(
     min_mz = np.floor(np.min([np.min(to_mz(s)) for s in mx_spectra if len(s)]))
     max_mz = np.ceil(np.max([np.max(to_mz(s)) for s in mx_spectra if len(s)]))
 
-    # WORKAROUND: MSIWarp splats each peak across 3 * sigma_1, which can cause low-coverage peaks
-    # to be overshadowed by higher-coverage peaks within a much wider range. Reduce sigma_1
-    # to compensate. Peaks' individual sigma_1 values will be used for the actual peak shape.
-    sigma_1 /= 3
-
     # MSIWarp's generate_mean_spectrum needs a temporary array to store a fuzzy histogram of peaks
     # with a distribution function that ensures the peak width is a constant number of bins
     # throughout the m/z range. The formula for this is different for each analyzer.
@@ -39,7 +34,7 @@ def _get_mean_spectrum(
     # silently fails. If it's set too high, it takes longer to run and there are console warnings.
     # Predict the required number of n_points so that neither of these conditions are hit.
     # A buffer of 10% + 1000 is added to compensate for numerical error
-    exp = {'tof': 1, 'orbitrap': 1.5, 'ft-icr': 2}[analyzer]
+    exp = {"tof": 1, "orbitrap": 1.5, "ft-icr": 2}[analyzer]
     density_samples = np.linspace(min_mz, max_mz, 100) ** exp * 0.25 * sigma_1
     n_points = int(
         (max_mz - min_mz) / np.average(density_samples, weights=1 / density_samples) * 1.1 + 1000
@@ -60,13 +55,13 @@ def _get_mean_spectrum(
 def make_spectra_df(spectra):
     return pd.DataFrame(
         {
-            'sp_i': np.concatenate(
+            "sp_i": np.concatenate(
                 [np.full(len(mzs), sp_i, dtype=np.uint32) for sp_i, mzs, ints in spectra]
             ),
-            'mz': np.concatenate([mzs for sp_i, mzs, ints in spectra]),
-            'ints': np.concatenate([ints for sp_i, mzs, ints in spectra]),
+            "mz": np.concatenate([mzs for sp_i, mzs, ints in spectra]),
+            "ints": np.concatenate([ints for sp_i, mzs, ints in spectra]),
         }
-    ).sort_values('mz')
+    ).sort_values("mz")
 
 
 def representative_spectrum(
@@ -84,7 +79,7 @@ def representative_spectrum(
         # Exclude peaks that only exist in small number of spectra, have high m/z variability
         # (which suggests that multiple peaks were grouped together), or are near other more
         # intense peaks
-        mean_spectrum = mean_spectrum[mean_spectrum.n_hits > 1]
+        # mean_spectrum = mean_spectrum[mean_spectrum.n_hits > 1]
         _ints = mean_spectrum.ints.values
         _mz = mean_spectrum.mz.values
         local_lo, local_hi = mass_accuracy_bound_indices(_mz, _mz, analyzer, sigma_1 * 2)
@@ -103,55 +98,55 @@ def representative_spectrum(
 
         mean_spectrum = sample_across_mass_range(mean_spectrum, peak_score, n_per_bin=500)
         logger.debug(
-            f'Denoising reduced peaks from {len(orig_mean_spectrum)} to {len(mean_spectrum)}'
+            f"Denoising reduced peaks from {len(orig_mean_spectrum)} to {len(mean_spectrum)}"
         )
 
     # Find the spectrum that's most similar to the background spectrum
-    mean_spectrum = mean_spectrum.rename(columns={'mz': 'mean_mz', 'ints': 'mean_ints'})
+    mean_spectrum = mean_spectrum.rename(columns={"mz": "mean_mz", "ints": "mean_ints"})
     spectrum_scores = {}
     processed_spectra = {}
-    for sp, grp in spectra_df.groupby('sp'):
-        joined = join_by_mz(mean_spectrum, 'mean_mz', grp, 'mz', analyzer, sigma_1, how='left')
+    for sp, grp in spectra_df.groupby("sp"):
+        joined = join_by_mz(mean_spectrum, "mean_mz", grp, "mz", analyzer, sigma_1, how="left")
         mz_tol = peak_width(joined.mz, analyzer, sigma_1) / 2
-        joined['mz_err'] = np.clip((joined.mean_mz - joined.mz.fillna(0)) / mz_tol, -1, 1)
+        joined["mz_err"] = np.clip((joined.mean_mz - joined.mz.fillna(0)) / mz_tol, -1, 1)
         a = joined.mean_ints
         b = joined.ints.fillna(0)
         mz_err = max(joined.mz_err.abs().sum(), 0.0001)
         # score = cosine_similarity(mean_ints, ints) / mz_err.sum()
         spectrum_scores[sp] = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)) / mz_err
         if denoise:
-            processed_spectra[sp] = joined[['sp', 'mz', 'ints']][~joined.ints.isna()]
+            processed_spectra[sp] = joined[["sp", "mz", "ints"]][~joined.ints.isna()]
         else:
             processed_spectra[sp] = grp
 
     # Return the best scoring spectrum
     best_sp = pd.Series(spectrum_scores).idxmax()
-    logger.debug(f'Choose representative spectrum: {best_sp}')
-    return processed_spectra[best_sp].sort_values('mz')
+    logger.debug(f"Choose representative spectrum: {best_sp}")
+    return processed_spectra[best_sp].sort_values("mz")
 
 
 def hybrid_mean_spectrum(spectra_df, analyzer, sigma_1, min_coverage=0):
     from msiwarp.util.warp import to_mz
 
     if not spectra_df.mz.is_monotonic_increasing:
-        spectra_df = spectra_df.sort_values('mz')
+        spectra_df = spectra_df.sort_values("mz")
 
     n_spectra = spectra_df.sp.nunique()
     mx_spectra = [
-        to_mx_peaks(grp.mz, grp.ints, sigma_1, sp, analyzer) for sp, grp in spectra_df.groupby('sp')
+        to_mx_peaks(grp.mz, grp.ints, sigma_1, sp, analyzer) for sp, grp in spectra_df.groupby("sp")
     ]
-    logger.debug(f'Converted {sum(map(len, mx_spectra))} peaks to mx.peak')
+    logger.debug(f"Converted {sum(map(len, mx_spectra))} peaks to mx.peak")
 
     mean_spectrum = _get_mean_spectrum(mx_spectra, analyzer, sigma_1)
     mean_spectrum_df = pd.DataFrame(
-        {'mz': to_mz(mean_spectrum), 'ints': np.float32(to_height(mean_spectrum))}
-    ).sort_values('mz')
-    logger.debug(f'MSIWarp generate_mean_spectrum returned {len(mean_spectrum_df)} peaks')
+        {"mz": to_mz(mean_spectrum), "ints": np.float32(to_height(mean_spectrum))}
+    ).sort_values("mz")
+    logger.debug(f"MSIWarp generate_mean_spectrum returned {len(mean_spectrum_df)} peaks")
 
     lo_mzs, hi_mzs = mass_accuracy_bounds(mean_spectrum_df.mz.values, analyzer, sigma_1)
 
-    lo_idxs = np.searchsorted(spectra_df.mz, lo_mzs, 'left')
-    hi_idxs = np.searchsorted(spectra_df.mz, hi_mzs, 'right')
+    lo_idxs = np.searchsorted(spectra_df.mz, lo_mzs, "left")
+    hi_idxs = np.searchsorted(spectra_df.mz, hi_mzs, "right")
     results = []
     for lo_idx, hi_idx, mz_tol, mx_mz, mx_ints, lo_mz, hi_mz in zip(
         lo_idxs,
@@ -176,19 +171,19 @@ def hybrid_mean_spectrum(spectra_df, analyzer, sigma_1, min_coverage=0):
                 ints_mean = sum(ints) / n_spectra
                 results.append(
                     {
-                        'mz': mz_mean,
-                        'mz_stddev': mz_stddev,
-                        'mz_mx': mx_mz,
-                        'mz_tol': mz_tol,
-                        'ints': ints_mean,
-                        'ints_stddev': np.sqrt(np.average((ints - ints_mean) ** 2)),
-                        'ints_mx': mx_ints,
-                        'coverage': n_hits / n_spectra,
-                        'n_hits': n_hits,
+                        "mz": mz_mean,
+                        "mz_stddev": mz_stddev,
+                        "mz_mx": mx_mz,
+                        "mz_tol": mz_tol,
+                        "ints": ints_mean,
+                        "ints_stddev": np.sqrt(np.average((ints - ints_mean) ** 2)),
+                        "ints_mx": mx_ints,
+                        "coverage": n_hits / n_spectra,
+                        "n_hits": n_hits,
                     }
                 )
 
-    logger.debug(f'Hybrid_mean_spectrum returned {len(results)} peaks (sigma_1: {sigma_1})')
+    logger.debug(f"Hybrid_mean_spectrum returned {len(results)} peaks (sigma_1: {sigma_1})")
 
     return pd.DataFrame(results)
 
@@ -202,7 +197,7 @@ def sample_across_mass_range(spectrum: pd.DataFrame, scores, n_bins=4, n_per_bin
     for i in range(n_bins):
         bin_mask = spectrum.mz.between(bin_edges[i], bin_edges[i + 1])
         idxs = np.argsort(scores[bin_mask])[-n_per_bin:]
-        logger.debug(f'Chose {len(idxs)} peaks from {bin_edges[i]:.0f}-{bin_edges[i + 1]:.0f}')
+        logger.debug(f"Chose {len(idxs)} peaks from {bin_edges[i]:.0f}-{bin_edges[i + 1]:.0f}")
         bins.append(spectrum[bin_mask].iloc[idxs])
 
-    return pd.concat(bins).sort_values('mz')
+    return pd.concat(bins).sort_values("mz")

metaspace/recal/msi_recal/recalibrate.py
@@ -8,6 +8,7 @@ import pandas as pd
 from pyimzml.ImzMLParser import ImzMLParser
 from pyimzml.ImzMLWriter import ImzMLWriter
 
+from msi_recal.evaluate import EvalPeaksCollector
 from msi_recal.params import RecalParams
 from msi_recal.passes.align_msiwarp import AlignMsiwarp
 from msi_recal.passes.align_ransac import AlignRansac
@@ -23,11 +24,11 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 TRANSFORM = {
-    'align_msiwarp': AlignMsiwarp,
-    'align_ransac': AlignRansac,
-    'recal_msiwarp': RecalMsiwarp,
-    'recal_ransac': RecalRansac,
-    'normalize': Normalize,
+    "align_msiwarp": AlignMsiwarp,
+    "align_ransac": AlignRansac,
+    "recal_msiwarp": RecalMsiwarp,
+    "recal_ransac": RecalRansac,
+    "normalize": Normalize,
 }
 
 
@@ -38,16 +39,16 @@ def get_spectra_df_from_parser(p: ImzMLParser, sp_idxs: Iterable[int]):
     for i in sp_idxs:
         mzs, ints = p.getspectrum(i)
         x, y, z = p.coordinates[i]
-        mask = ints > 0
-        mzs = mzs[mask]
-        ints = ints[mask]
-        peaks_dfs.append(pd.DataFrame({'sp': i, 'mz': mzs, 'ints': ints.astype(np.float32)}))
+        mask = (ints > 0) & (mzs > 200) & (mzs < 900)
+        mzs = mzs[mask].astype(np.float64)
+        ints = ints[mask].astype(np.float32)
+        peaks_dfs.append(pd.DataFrame({"sp": i, "mz": mzs, "ints": ints}))
         spectra.append((i, x, y, z, np.min(mzs), np.max(mzs), np.sum(ints)))
 
     peaks_df = pd.concat(peaks_dfs)
     spectra_df = pd.DataFrame(
-        spectra, columns=['sp', 'x', 'y', 'z', 'mz_lo', 'mz_hi', 'tic']
-    ).set_index('sp')
+        spectra, columns=["sp", "x", "y", "z", "mz_lo", "mz_hi", "tic"]
+    ).set_index("sp")
 
     return peaks_df, spectra_df
 
@@ -61,6 +62,9 @@ def get_sample_spectra_df_from_parser(p: ImzMLParser, n_samples=200):
 
 def build_pipeline(sample_peaks_df: pd.DataFrame, params: RecalParams):
     models = []
+    stages = [
+        ('initial', sample_peaks_df),
+    ]
     for tf_name, *tf_args in params.transforms:
         logger.info(f'Fitting {tf_name} model')
         assert tf_name in TRANSFORM, f'Unrecognized transform "{tf_name}"'
@@ -70,8 +74,23 @@ def build_pipeline(sample_peaks_df: pd.DataFrame, params: RecalParams):
         sample_peaks_df = tf.predict(sample_peaks_df)
 
         models.append(tf)
+        stages.append((tf_name, sample_peaks_df))
 
-    return models
+    # Hacky progress report
+    eval = EvalPeaksCollector(sample_peaks_df, params)
+    for stage_name, stage_df in stages:
+        eval.collect_peaks(stage_df, stage_name)
+
+    __import__('__main__').eval = eval
+
+    logger.debug(pd.DataFrame({
+        stage_name: eval.get_stats(stage_name).abs().mean()
+        for stage_name, stage_df in stages
+    }))
+
+    eval.reset()
+
+    return models, eval
 
 
 def _imzml_writer_process(output_path, queue):
@@ -100,10 +119,10 @@ def process_imzml_file(
     if debug_path == 'infer':
         debug_path = Path(f'{input_path.parent}/{input_path.stem}_debug/')
 
-    p = ImzMLParser(str(input_path))
+    p = ImzMLParser(str(input_path), parse_lib="ElementTree")
     sample_peaks_df, sample_spectra_df = get_sample_spectra_df_from_parser(p, n_samples=samples)
 
-    models = build_pipeline(sample_peaks_df, params)
+    models, eval = build_pipeline(sample_peaks_df, params)
 
     writer_queue = JoinableQueue(2)
     writer_process = Process(target=_imzml_writer_process, args=(output_path, writer_queue))
@@ -113,12 +132,20 @@ def process_imzml_file(
 
     try:
         chunk_size = 1000
-        sp_n = min(limit, len(p.coordinates)) if limit else len(p.coordinates)
+        if limit:
+            skip = max(0, (len(p.coordinates) - limit) // 2)
+            sp_n = min(limit, len(p.coordinates) - skip)
+        else:
+            skip = 0
+            sp_n = len(p.coordinates)
 
-        for start_i in range(0, sp_n, chunk_size):
-            end_i = min(start_i + chunk_size, sp_n)
-            logger.debug(f'Reading spectra {start_i}-{end_i} out of {sp_n}')
+        for start_i in range(skip, skip + sp_n, chunk_size):
+            end_i = min(start_i + chunk_size, len(p.coordinates))
+            if start_i >= end_i:
+                continue
+            logger.debug(f'Reading spectra {start_i}-{end_i} out of {len(p.coordinates)}')
             peaks_df, spectra_df = get_spectra_df_from_parser(p, np.arange(start_i, end_i))
+            eval.collect_peaks(peaks_df, 'before')
             # Convert to tuples, because spectra_df.loc coalesces everything into floats
             spectra_dict = {row[0]: row for row in spectra_df.itertuples()}
             all_spectra_dfs.append(spectra_df)
@@ -126,6 +153,8 @@ def process_imzml_file(
             logger.info(f'Transforming spectra {start_i}-{end_i}')
             for model in models:
                 peaks_df = model.predict(peaks_df)
+
+            eval.collect_peaks(peaks_df, 'after')
 
             writer_job = []
             for sp, grp in peaks_df.groupby('sp'):
@@ -160,3 +189,5 @@ def process_imzml_file(
                     model.save_debug(all_spectra_df, str(debug_path / f'{i}_{transform_name}'))
             except:
                 logger.warning(f'{transform_name} error', exc_info=True)
+
+        eval.get_report().to_csv(debug_path / 'evaluation_peaks.csv')