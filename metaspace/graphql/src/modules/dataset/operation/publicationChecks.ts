d98e4d49b867c853bc21c481dd23c49f94c36a2e
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2021-07-08 14:43:48 +0200

    Remove the troublesome dataloader completely

--- metaspace/graphql/src/modules/dataset/operation/publicationChecks.ts
+++ metaspace/graphql/src/modules/dataset/operation/publicationChecks.ts
@@ -2,46 +2,37 @@ import { DatasetProject as DatasetProjectModel } from '../model'
 import { PublicationStatusOptions as PSO } from '../../project/Publishing'
 import { UserError } from 'graphql-errors'
 import { PublicationStatus } from '../../../binding'
-import * as DataLoader from 'dataloader'
 import { Context } from '../../../context'
-import * as _ from 'lodash'
 import { EntityManager } from 'typeorm'
+import { ProjectSourceRepository } from '../../project/ProjectSourceRepository'
 
-const fetchDatasetProjectsInStatusUncached = async(
-  entityManager: EntityManager, datasetIds: string[], statuses: PublicationStatus[]
+/**
+ * This function has two big differences to ProjectSourceRepository.findProjectsByDatasetId:
+ * 1. It doesn't do permissions checks, which ensures that private project in the "under review" status can prevent
+ *    dataset deletion
+ * 2. It doesn't use `Context`, allowing it to be used inside `getDatasetForEditing` without needing to rewrite some
+ *    codepaths
+ */
+const fetchDatasetProjectsInStatus = async(
+  entityManager: EntityManager, datasetId: string, statuses: PublicationStatus[]
 ) => {
-  const rows = await entityManager.createQueryBuilder(DatasetProjectModel, 'dsProj')
+  return await entityManager.createQueryBuilder(DatasetProjectModel, 'dsProj')
     .leftJoinAndSelect('dsProj.project', 'proj')
-    .where('dsProj.datasetId = ANY(:datasetIds)', { datasetIds })
+    .where('dsProj.datasetId = :datasetId', { datasetId })
     .andWhere('proj.publicationStatus = ANY(:statuses)', { statuses })
     .getMany()
-
-  const groupedRows = _.groupBy(rows, 'datasetId')
-  return datasetIds.map(id => groupedRows[id] || [])
-}
-
-const fetchDatasetProjectsInStatus = async(
-  ctx: Context, datasetId: string, statuses: PublicationStatus[]
-): Promise<DatasetProjectModel[]> => {
-  const statusCacheKey = statuses.join(',')
-  const dataLoader = ctx.contextCacheGet('fetchDatasetProjectsInStatusDataLoader', [statusCacheKey],
-    (statusCacheKey) => {
-      const statuses = statusCacheKey.split(',') as PublicationStatus[]
-      return new DataLoader(
-        (datasetIds: string[]) => fetchDatasetProjectsInStatusUncached(ctx.entityManager, datasetIds, statuses)
-      )
-    })
-  return dataLoader.load(datasetId)
 }
 
 export const isDatasetInPublicationStatus = async(ctx: Context, datasetId: string, statuses: PublicationStatus[]) => {
-  return (await fetchDatasetProjectsInStatus(ctx, datasetId, statuses)).length > 0
+  const projects = await ctx.entityManager.getCustomRepository(ProjectSourceRepository)
+    .findProjectsByDatasetId(ctx, datasetId)
+  return projects.some(p => statuses.includes(p.publicationStatus))
 }
 
 export const checkProjectsPublicationStatus = async(
   entityManager: EntityManager, datasetId: string, statuses: PublicationStatus[]
 ) => {
-  const [dsProjectPublished] = (await fetchDatasetProjectsInStatusUncached(entityManager, [datasetId], statuses))
+  const dsProjectPublished = await fetchDatasetProjectsInStatus(entityManager, datasetId, statuses)
   if (dsProjectPublished.length > 0) {
     const projectStatusList = dsProjectPublished
       .map(dp => ({ projectId: dp.projectId, status: dp.project.publicationStatus }))
@@ -55,10 +46,9 @@ export const checkProjectsPublicationStatus = async(
 export const checkNoPublishedProjectRemoved = async(
   entityManager: EntityManager, datasetId: string, updatedProjectIds: string[]
 ) => {
-  const [dsProjects] = await fetchDatasetProjectsInStatusUncached(
-    entityManager, [datasetId], [PSO.PUBLISHED, PSO.UNDER_REVIEW]
-  )
-  const removedDsProject = dsProjects.find(dsProj => !updatedProjectIds.includes(dsProj.projectId))
+  const removedDsProject = (
+    await fetchDatasetProjectsInStatus(entityManager, datasetId, [PSO.PUBLISHED, PSO.UNDER_REVIEW])
+  ).find(dsProj => !updatedProjectIds.includes(dsProj.projectId))
 
   if (removedDsProject) {
     throw new UserError(JSON.stringify({
