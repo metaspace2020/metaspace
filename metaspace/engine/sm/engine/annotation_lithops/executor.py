a7d9da5a5da691e6c87dc9c27ba00c14e141e9f7
Sergii Mamedov <finve.ua@gmail.com>
2021-07-29 12:08:56 +0300

    Add .stop() for VM

--- metaspace/engine/sm/engine/annotation_lithops/executor.py
+++ metaspace/engine/sm/engine/annotation_lithops/executor.py
@@ -306,7 +306,7 @@ class Executor:
                             # Dismantle & wait for it to stop while the mutex is still active
                             # to avoid a race condition, as there's still some instability if a
                             # second request tries to start the VM while it is still stopping.
-                            executor.compute_handler.dismantle()
+                            executor.compute_handler.backend.master.stop()
                 except Exception as exc:
                     exception = exc
 
