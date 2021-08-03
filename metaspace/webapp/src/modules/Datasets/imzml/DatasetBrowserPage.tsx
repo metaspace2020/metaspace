6c74acf9254e263124fb6bdc60ded94f2525a1ae
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2021-08-03 17:08:34 -0300

    [DsBrowserPage] updated echarts version, added custom restore and added label overlap handler

--- metaspace/webapp/src/modules/Datasets/imzml/DatasetBrowserPage.tsx
+++ metaspace/webapp/src/modules/Datasets/imzml/DatasetBrowserPage.tsx
@@ -10,7 +10,6 @@ import 'echarts/lib/component/grid'
 import 'echarts/lib/component/legend'
 import 'echarts/lib/component/dataZoom'
 import 'echarts/lib/component/markPoint'
-import 'echarts/lib/component/markArea'
 import './DatasetBrowserPage.scss'
 import { useQuery } from '@vue/apollo-composable'
 import { getDatasetByIdQuery, GetDatasetByIdQuery } from '../../../api/dataset'
@@ -103,13 +102,28 @@ export default defineComponent<DatasetBrowserProps>({
         tooltip: {
           show: true,
           formatter: function(value: any) {
-            return value.data.label
+            return value.data.tooltip
           },
         },
         toolbox: {
           feature: {
-            restore: {
+            myTool1: {
+              show: true,
               title: 'Restore',
+              icon:
+                'path://M512 981.333333c-209.866667 0-396.693333-126.026667-466.293333-314.08a35.52 35.52 0 0 1 '
+                + '23.626666-44.426666 38.613333 38.613333 0 0 1 48 20.693333c58.666667 158.933333 217.013333 '
+                + '265.493333 394.666667 265.6s336-106.666667 394.666667-266.133333a37.6 37.6 0 0 1 '
+                + '28.853333-23.626667 38.986667 38.986667 0 0 1 35.786667 11.946667 34.773333 34.773333 '
+                + '0 0 1 7.146666 35.36c-69.386667 188.373333-256.48 314.666667-466.453333 314.666666z '
+                + 'm431.36-574.08a37.92 37.92 0 0 1-35.946667-24.266666C849.386667 222.56 690.613333 114.88 '
+                + '512 114.72S174.72 222.346667 116.746667 382.773333A38.72 38.72 0 0 1 69.333333 403.733333a35.786667 '
+                + '35.786667 0 0 1-24.106666-44.373333C113.333333 169.866667 301.013333 42.666667 512 '
+                + '42.666667s398.666667 127.306667 467.146667 316.96a34.56 34.56 0 0 1-4.906667 32.64 '
+                + '38.933333 38.933333 0 0 1-30.88 14.986666z',
+              onclick: () => {
+                handleZoomReset()
+              },
             },
             dataZoom: {
               title: {
@@ -181,13 +195,17 @@ export default defineComponent<DatasetBrowserProps>({
             name: 'Unannotated',
             type: 'bar',
             data: [],
+            label: {
+              show: true,
+              position: 'top',
+              formatter: '{b}',
+            },
+            labelLayout: {
+              hideOverlap: true,
+            },
             barWidth: 2,
             itemStyle: {
               color: 'red',
-
-            },
-            markArea: {
-              data: [],
             },
             markPoint: {
               symbol: 'circle',
@@ -735,9 +753,8 @@ export default defineComponent<DatasetBrowserProps>({
 
       const auxOptions = state.chartOptions
       const data = []
-      const markAreaData = []
-      const markPointData: any[] = []
       const annotatedTheoreticalMzs = annotatedPeaks.value
+      const markPointData: any[] = []
       let minX
       let maxX
       const maxIntensity = Math.max(...state.sampleData[0].ints)
@@ -767,7 +784,8 @@ export default defineComponent<DatasetBrowserProps>({
 
         if (state.peakFilter === PEAK_FILTER.ALL && !isAnnotated) { // add unnanotated peaks
           data.push({
-            label: tooltip,
+            name: xAxis.toFixed(4),
+            tooltip,
             mz: xAxis,
             value: [xAxis, yAxis],
             itemStyle: {
@@ -784,40 +802,19 @@ export default defineComponent<DatasetBrowserProps>({
               color: isAnnotated ? 'blue' : 'red',
             },
           })
-          markAreaData.push([
-            {
-              label: tooltip,
-              mz: xAxis,
-              xAxis: xAxis * 1.000003,
-              yAxis: 0,
-              itemStyle: {
-                color: isAnnotated ? 'blue' : 'red',
-                opacity: 0.3,
-              },
-            },
-            {
-              label: tooltip,
-              mz: xAxis,
-              xAxis: xAxis * 0.999997,
-              yAxis: yAxis,
-              itemStyle: {
-                color: isAnnotated ? 'green' : 'blue',
-                opacity: 0.3,
-              },
-
-            },
-          ])
         }
 
         if (isAnnotated) {
           data.push({
-            label: tooltip,
+            name: xAxis.toFixed(4),
+            tooltip,
             mz: xAxis,
             value: [xAxis, yAxis],
             itemStyle: {
               color: isAnnotated ? 'blue' : 'red',
             },
           })
+
           markPointData.push({
             label: tooltip,
             mz: xAxis,
@@ -827,29 +824,6 @@ export default defineComponent<DatasetBrowserProps>({
               color: isAnnotated ? 'blue' : 'red',
             },
           })
-          markAreaData.push([
-            {
-              label: tooltip,
-              mz: xAxis,
-              xAxis: xAxis * 1.000003,
-              yAxis: 0,
-              itemStyle: {
-                color: isAnnotated ? 'blue' : 'red',
-                opacity: 0.3,
-              },
-            },
-            {
-              label: tooltip,
-              mz: xAxis,
-              xAxis: xAxis * 0.999997,
-              yAxis: yAxis,
-              itemStyle: {
-                color: isAnnotated ? 'green' : 'blue',
-                opacity: 0.3,
-              },
-
-            },
-          ])
         }
       }
 
@@ -857,9 +831,8 @@ export default defineComponent<DatasetBrowserProps>({
       auxOptions.xAxis.max = maxX
       auxOptions.yAxis.name = state.scaleIntensity ? 'Relative Intensity' : 'Intensity'
       auxOptions.yAxis.max = state.scaleIntensity ? 100 : maxIntensity
-      auxOptions.series[0].data = data
-      auxOptions.series[0].markArea.data = markAreaData
       auxOptions.series[0].markPoint.data = markPointData
+      auxOptions.series[0].data = data
       state.chartOptions = auxOptions
       handleZoomReset()
     }
@@ -959,7 +932,6 @@ export default defineComponent<DatasetBrowserProps>({
 
     return () => {
       const isEmpty = state.x === undefined && state.y === undefined
-
       return (
         <div class={'dataset-browser-container'}>
           <div class={'dataset-browser-wrapper w-full lg:w-1/2'}>
