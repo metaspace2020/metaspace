9dd018acfc333a406891930520fa63eaa67b26ae
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2021-09-01 15:28:20 -0300

    Added npy parser lib compatible with safari

--- metaspace/webapp/src/lib/npyHandler.ts
+++ metaspace/webapp/src/lib/npyHandler.ts
@@ -1,12 +1,12 @@
 // @ts-ignore
-import npyjs from 'npyjs'
+import { fromArrayBuffer } from 'numpy-parser'
 
 export const readNpy = async(npyFile: string) => {
   try {
-    // eslint-disable-next-line new-cap
-    const n = new npyjs()
-    const { data, shape } = await n.load(npyFile)
-    return { data: data, shape }
+    const response = await fetch(npyFile)
+    const arrayBuffer = await response.arrayBuffer()
+    const { data, shape } = fromArrayBuffer(arrayBuffer)
+    return { data, shape }
   } catch (e) {
     return new Promise<any>((resolve, reject) => reject(Error("Couldn't parse file")))
   }
