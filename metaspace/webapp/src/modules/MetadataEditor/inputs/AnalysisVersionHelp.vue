60f3ad5a66bedafaa154cd5d1842f74e64cf2964
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2022-01-31 21:27:10 +0100

    Update text in AnalysisVersionHelp

--- metaspace/webapp/src/modules/MetadataEditor/inputs/AnalysisVersionHelp.vue
+++ metaspace/webapp/src/modules/MetadataEditor/inputs/AnalysisVersionHelp.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="analysis-version-help">
-    Selects the version of the METASPACE analysis pipeline to use. When comparing data between multiple datasets,
-    it is recommended to process all datasets with the same analysis version. Otherwise, select the latest stable version.
-    <h4>Changes between versions:</h4>
+    When comparing data between multiple datasets, it is recommended to process all datasets with the same
+    analysis version. Otherwise, select the latest version for best results.
+    <h4>Changes:</h4>
     <h5 v-if="showV15">
-      Version 1.5 (unreleased version used for internal testing):
+      v1.5 (Prototype for higher RPs):
     </h5>
     <ul v-if="showV15">
       <li>Improved modeling of instrument mass accuracy at higher m/z values for TOF and Orbitrap.</li>
       <li>Improved isotopic peak calculation algorithm.</li>
     </ul>
-    <h5>Version 2:</h5>
+    <h5>v2 (ML-powered MSM):</h5>
     <ul>
-      <li>New ML-powered model for scoring annotations.</li>
       <li>New metrics for evaluating annotations' mass accuracy.</li>
+      <li>New ML-powered model for annotation scoring.</li>
       <li>Fine-grained reporting of FDR values.</li>
     </ul>
   </div>
