cfaf42317134fc0130471c33b8c7026aef4700dd
Sergii Mamedov <finve.ua@gmail.com>
2022-02-15 11:31:09 +0100

    Linter

--- metaspace/engine/scripts/populate_molecular_db_table.py
+++ metaspace/engine/scripts/populate_molecular_db_table.py
@@ -9,18 +9,20 @@ def save_additional_info_to_db(db_id, user_id, input_path):
     conf = SMConfig.get_conf()
     with ConnectionPool(conf['db']):
         db = DB()
-        if db.select_one('SELECT * FROM molecular_db WHERE id = %s', (db_id, )):
+        if db.select_one('SELECT * FROM molecular_db WHERE id = %s', (db_id,)):
             print(f'Updating existing molecular database {db_id}')
             DB().alter(
                 'UPDATE molecular_db SET user_id = %s, input_path = %s WHERE id = %s',
-                (user_id, input_path, db_id)
+                (user_id, input_path, db_id),
             )
         else:
             print(f'Specified molecular database {db_id} does not exist.')
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Populate user_id and input_path into a molecular_db table.')
+    parser = argparse.ArgumentParser(
+        description='Populate user_id and input_path into a molecular_db table.'
+    )
     parser.add_argument('filename', type=str, help='Input file with new user_id and input_path.')
     args = parser.parse_args()
 
