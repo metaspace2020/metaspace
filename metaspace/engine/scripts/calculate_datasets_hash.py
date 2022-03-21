75b3395c143b457e65954044a6880c579392f3cf
Sergii Mamedov <finve.ua@gmail.com>
2022-03-21 10:48:29 +0100

    Linter

--- metaspace/engine/scripts/calculate_datasets_hash.py
+++ metaspace/engine/scripts/calculate_datasets_hash.py
@@ -13,15 +13,15 @@ logging.basicConfig(
 )
 
 
-def calc_hash(object, chunk_size=1024 * 1024):
+def calc_hash(s3_object, chunk_size=1024 * 1024):
     """Calculate md5 and sha256 hash for an object chunk by chunk."""
     md5_hash = hashlib.md5()
     sha_hash = hashlib.sha256()
-    chunk = object.read(chunk_size)
+    chunk = s3_object.read(chunk_size)
     while chunk:
         md5_hash.update(chunk)
         sha_hash.update(chunk)
-        chunk = object.read(chunk_size)
+        chunk = s3_object.read(chunk_size)
 
     return {
         'md5': md5_hash.hexdigest(),
@@ -43,30 +43,33 @@ def main():
 
     data = []
     df = pd.read_csv(args.input_file, keep_default_na=False)
-    for it in df.sort_values(by='Size').to_dict('records'):
-        if it['Key'].startswith('databases'):
+    for file in df.sort_values(by='Size').to_dict('records'):
+        if file['Key'].startswith('databases'):
             continue
-        filename = it['Key'].split('/')[-1]
+        filename = file['Key'].split('/')[-1]
         if filename.lower().endswith('ibd') or filename.lower().endswith('imzml'):
             try:
-                resp = conn.get_object(Bucket=args.bucket, Key=it['Key'], VersionId=it['VersionId'])
+                resp = conn.get_object(
+                    Bucket=args.bucket, Key=file['Key'], VersionId=file['VersionId']
+                )
                 start = time.time()
                 item = {}
                 item.update(calc_hash(resp['Body']))
                 item.update({'calc_time': round(time.time() - start, 3)})
                 item.update(
                     {
-                        'size': it['Size'],
-                        'version_id': it['VersionId'],
-                        'key': it['Key'],
+                        'size': file['Size'],
+                        'version_id': file['VersionId'],
+                        'key': file['Key'],
                     }
                 )
                 data.append(item)
                 logging.info(
-                    f'{item["md5"]} {item["sha256"]} {item["calc_time"]:>9.3f} {item["size"]} {item["version_id"]} {item["key"]}'
+                    f'{item["md5"]} {item["sha256"]} {item["calc_time"]:>9.3f}\
+                     {item["size"]} {item["version_id"]} {item["key"]}'
                 )
             except botocore.exceptions.ClientError:
-                logging.error(f'{it["Key"]}')
+                logging.error(f'{file["Key"]}')
 
     df = pd.DataFrame.from_records(data)
     df.to_csv(args.output_file, index=False, sep='\t')
