a0b796a7366666aaaa4fe6b538b4eb46f0399ba3
Sergii Mamedov <finve.ua@gmail.com>
2022-05-17 14:28:55 +0200

    Return back image from get_intensity_by_mz_ppm function

--- metaspace/engine/sm/rest/api.py
+++ metaspace/engine/sm/rest/api.py
@@ -50,7 +50,9 @@ class DatasetBrowser:
 
         self._coordinates = self._get_coordinates()
         self._mz_index = self._get_mz_index()
-        self.mz_peaks = self._get_mz_peaks()
+        self._mz_peaks = self._get_mz_peaks()
+
+        self._mz_image, self._alpha = self._create_mz_image()
 
     def _get_input_path_uuid(self):
         with ConnectionPool(self._sm_config['db']):
@@ -87,13 +89,14 @@ class DatasetBrowser:
         # previous chunk actually includes value
         mz_low_chunk_idx -= 1
 
-        chunk_size = 4 * 1024
+        chunk_size = 3 * 4 * 1024  # num of elements, size in bytes, chunk record size
         offset = mz_low_chunk_idx * chunk_size
         bytes_to_read = (mz_high_chunk_idx - mz_low_chunk_idx + 1) * chunk_size
 
         key = f'{self._uuid}/peaks_sorted_by_mz.bin'
+
         bytes = self._s3_client.get_object(
-            Bucket=self._bucket, Key=key, Range=f'bytes={offset}-{offset+bytes_to_read}'
+            Bucket=self._bucket, Key=key, Range=f'bytes={offset}-{offset+bytes_to_read-1}'
         )['Body'].read()
         mz_chunks_array = np.frombuffer(bytes, dtype='f').reshape(-1, 3)
 
@@ -103,17 +106,67 @@ class DatasetBrowser:
 
         return mz_peaks
 
+    def _create_mz_image(self):
+        min_x, min_y = np.amin(self._coordinates, axis=0)
+        max_x, max_y = np.amax(self._coordinates, axis=0)
+        nrows, ncols = max_y - min_y + 1, max_x - min_x + 1
+
+        alpha = np.zeros(shape=(nrows, ncols))
+        all_xs, all_ys = zip(*self._coordinates)
+        all_xs -= min_x
+        all_ys -= min_y
+        alpha[all_ys, all_xs] = 1
+
+        image_coord_idxs = self._mz_peaks[:, 2].astype("i")
+        xs = all_xs[image_coord_idxs]
+        ys = all_ys[image_coord_idxs]
+        mz_image = np.zeros(shape=(nrows, ncols))
+        np.add.at(mz_image, [ys, xs], self._mz_peaks[:, 1])  # warning
+        mz_max = mz_image.max()
+        mz_min = mz_image.min()
+        if mz_image.max() > 0:
+            mz_image /= mz_image.max()
+
+        return mz_image, alpha
+
+    def _create_rgba_image(self):
+        from matplotlib import pyplot as plt
+
+        rgba_image = plt.get_cmap("gray")(self._mz_image)
+        rgba_image[:, :, 3] = self._alpha
+
+        return
+
 
 @app.post('/v1/imzml_browser/search')
 def get_intensity_by_mz_ppm():
     try:
         logger.info("IMZML BROWSER")
         params = body_to_json(bottle.request)
+        logger.info(type(params))
+        logger.info(params['ds_id'])
+        logger.info(params['mz_low'])
+        logger.info(params['mz_high'])
         assert params.get('ds_id')
         assert params.get('mz_low')
         assert params.get('mz_high')
 
-        return {'a': 1}
+        import io
+        import PIL
+        from bottle import HTTPResponse
+        from matplotlib import pyplot as plt
+
+        dataset = DatasetBrowser(params['ds_id'], params['mz_low'], params['mz_high'])
+
+        rgba_image = plt.get_cmap("gray")(dataset._mz_image)
+        rgba_image[:, :, 3] = dataset._alpha
+
+        image = PIL.Image.fromarray((rgba_image * 255).astype(np.uint8), mode='RGBA')
+        fp = io.BytesIO()
+        image.save(fp, format="PNG")
+        fp.seek(0)
+        return HTTPResponse(fp, **{'Content-Type': 'image/png'})
+
     except Exception as e:
         logger.warning(e)
         return make_response(INTERNAL_ERROR)
