c1c724f1b17c34a113d3b27fdd56eba36d85b982
lmacielvieira <lucas.maciel.vieira@gmail.com>
2022-06-24 11:38:26 +0200

    added migration file

---
+++ metaspace/graphql/src/migrations/1655977710255-OpticalImageTransform.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class OpticalImageTransform1655977710255 implements MigrationInterface {
+    name = 'OpticalImageTransform1655977710255'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "public"."dataset" ADD "optical_image_transform" double precision array`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "public"."dataset" DROP COLUMN "optical_image_transform"`);
+    }
+
+
+}
