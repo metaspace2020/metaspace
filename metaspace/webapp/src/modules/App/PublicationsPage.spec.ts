3f8998e5ffdec894bb3bcba2876ce6cb29e6a2d1
lmacielvieira <lucas.maciel.vieira@gmail.com>
2022-07-11 15:56:39 +0200

    [PublicationPage] Updated other publications to scholar link/added snapshot test

---
+++ metaspace/webapp/src/modules/App/PublicationsPage.spec.ts
@@ -0,0 +1,19 @@
+import { mount } from '@vue/test-utils'
+import PublicationsPage from './PublicationsPage.vue'
+import router from '../../router'
+import store from '../../store/index'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { sync } from 'vuex-router-sync'
+
+Vue.use(Vuex)
+sync(store, router)
+
+describe('PublicationsPage', () => {
+  it('should match snapshot (logged out)', async() => {
+    const wrapper = mount(PublicationsPage, { store, router })
+    await Vue.nextTick()
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
