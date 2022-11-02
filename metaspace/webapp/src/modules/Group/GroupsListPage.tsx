02dd06d49bd2ee6157eb7f96c1084668723a6b78
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2022-11-02 11:14:23 +0100

    [GroupsPage] removed duplicate groups

--- metaspace/webapp/src/modules/Group/GroupsListPage.tsx
+++ metaspace/webapp/src/modules/Group/GroupsListPage.tsx
@@ -1,11 +1,11 @@
 import { computed, defineComponent, reactive } from '@vue/composition-api'
-import './GroupsListPage.scss'
 import { Button, Input } from '../../lib/element-ui'
-import { calculateMzFromFormula, isFormulaValid } from '../../lib/formulaParser'
 import { useQuery } from '@vue/apollo-composable'
 import { currentUserRoleQuery, CurrentUserRoleResult } from '../../api/user'
 import { getUserGroupsQuery, ViewGroupResult } from '../../api/group'
+import { uniqBy } from 'lodash-es'
 import GroupsListItem from './GroupsListItem'
+import './GroupsListPage.scss'
 
 interface GroupListPageProps {
   className: string
@@ -95,7 +95,7 @@ export default defineComponent<GroupListPageProps>({
               {
                 groups.value?.length > 0
                 && !groupsLoading.value
-                && groups.value?.map((group: any) => {
+                && uniqBy((groups.value || []) as any[], 'id').map((group: any) => {
                   return <GroupsListItem
                     id={group.id}
                     name={group.name}
