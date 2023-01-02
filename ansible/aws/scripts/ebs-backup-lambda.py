0b6b78cfe53f224972dcf23569fdca82d2403325
Sergii Mamedov <finve.ua@gmail.com>
2023-01-02 14:50:25 +0100

    Add monthly snapshots

--- ansible/aws/scripts/ebs-backup-lambda.py
+++ ansible/aws/scripts/ebs-backup-lambda.py
@@ -48,6 +48,9 @@ def backup_region(ec2, region, today):
     to_tag_mount_point = collections.defaultdict(list)
 
     for instance in instances:
+        if today.day == 1:
+            # keep snapshots taken on the first day of month
+            retention_days = get_tag_value(instance, 'retention_monthly', default=180)
         if today.strftime('%A') == 'Sunday':
             # keep snapshots taken on Sunday for 'retention_weekly' days
             retention_days = get_tag_value(instance, 'retention_weekly', default=7)
