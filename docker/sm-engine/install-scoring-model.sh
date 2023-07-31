84ad7558bea2ff923d5cad177cb8dad7aecdf6ae
Sergii Mamedov <finve.ua@gmail.com>
2023-07-31 16:58:52 +0200

    Update scientific test (#1404)
    * Change catboost model file
    
    * Enable test-engine-sci test
    
    * Change reference values
    
    * Disable test-engine-sci test for CircleCI

--- docker/sm-engine/install-scoring-model.sh
+++ docker/sm-engine/install-scoring-model.sh
@@ -3,4 +3,4 @@ cd /opt/dev/metaspace/metaspace/engine
 pip install -qr requirements.txt
 pip install -e .
 
-python -m scripts.import_scoring_model v3_default ../scoring-models/v3_default/model-2022-01-05T13-45-26.947188-416b1311.cbm sm-engine-dev
+python -m scripts.import_scoring_model v3_default "../scoring-models/v3_default/v2.20230517_(METASPACE-ML).cbm" sm-engine-dev
