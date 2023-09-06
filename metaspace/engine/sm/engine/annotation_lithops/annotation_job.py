e1a65cfccba2d67f49037e3b0cc096abfc1f194e
Lucas Maciel Vieira <35172605+lmacielvieira@users.noreply.github.com>
2023-09-06 12:06:10 +0200

    Handling deleted dbs on dataset reprocessing (#1415)
    * [Annotation] checking if db exists before procceding to annotation
    
    * [Annotation] removing deleted dbs from dataset config on reprocess
    
    * [Annotation] added logging to deleted db removal

--- metaspace/engine/sm/engine/annotation_lithops/annotation_job.py
+++ metaspace/engine/sm/engine/annotation_lithops/annotation_job.py
@@ -10,6 +10,7 @@ from ibm_boto3.s3.transfer import TransferConfig, MB
 from lithops.storage import Storage
 from lithops.storage.utils import StorageNoSuchKeyError, CloudObject
 
+from sm.engine import molecular_db
 from sm.engine.annotation.diagnostics import (
     extract_dataset_diagnostics,
     add_diagnostics,
@@ -128,6 +129,11 @@ def _upload_moldbs_from_db(moldb_ids, storage, sm_storage):
     bucket, prefix = sm_storage['moldb']
     # Sort the moldbs because the centroids cache key is affected by their order
     for moldb_id in sorted(moldb_ids):
+        try:
+            molecular_db.find_by_id(moldb_id)
+        except Exception:  # db does not exist, continue to next
+            continue
+
         key = f'{prefix}/{moldb_id}'
         try:
             storage.head_object(bucket, key)
@@ -315,8 +321,23 @@ class ServerAnnotationJob:
         # TODO: Only run missing moldbs
         del_jobs(self.ds)
         moldb_to_job_map = {}
+        moldb_to_be_removed = []
+
         for moldb_id in self.ds.config['database_ids']:
-            moldb_to_job_map[moldb_id] = insert_running_job(self.ds.id, moldb_id)
+            try:
+                molecular_db.find_by_id(moldb_id)
+                moldb_to_job_map[moldb_id] = insert_running_job(self.ds.id, moldb_id)
+            except Exception:  # db does not exist, continue to next
+                moldb_to_be_removed.append(moldb_id)
+                logger.warning(f'Removed db {moldb_id} from {self.ds.id}')
+                continue
+
+        if len(moldb_to_be_removed) > 0:  # remove non-existing moldbs from ds
+            self.ds.config['database_ids'] = [
+                x for x in self.ds.config['database_ids'] if x not in moldb_to_be_removed
+            ]
+            self.ds.save(self.db)
+
         self.perf.add_extra_data(moldb_ids=list(moldb_to_job_map.keys()))
 
         n_peaks = self.ds.config['isotope_generation']['n_peaks']
