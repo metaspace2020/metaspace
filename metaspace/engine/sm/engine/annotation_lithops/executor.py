923f6eec3770b302c5b8fcfd329fa66e05457fb4
Sergii Mamedov <finve.ua@gmail.com>
2023-10-08 19:07:23 +0200

    Change docker image version for VPC

--- metaspace/engine/sm/engine/annotation_lithops/executor.py
+++ metaspace/engine/sm/engine/annotation_lithops/executor.py
@@ -24,7 +24,7 @@ TRet = TypeVar('TRet')
 #: manually updating their config files every time it changes. The image must be public on
 #: Docker Hub, and can be rebuilt using the scripts/Dockerfile in `engine/docker/lithops_ibm_cf`.
 #: Note: sci-test changes this constant to force local execution without docker
-RUNTIME_VPC = 'metaspace2020/metaspace-lithops:3.0.1.a'
+RUNTIME_VPC = 'metaspace2020/metaspace-lithops:3.0.1.b'
 RUNTIME_CE = 'metaspace2020/metaspace-lithops-ce:3.0.1.b'
 MEM_LIMITS = {
     'localhost': 32 * 1024,
