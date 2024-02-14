a53b86350d7b05ee96c9a8e174d1730d7563eca4
Sergii Mamedov <finve.ua@gmail.com>
2024-02-14 10:41:36 +0100

    Bugfix

--- metaspace/engine/sm/engine/postprocessing/cloudwatch.py
+++ metaspace/engine/sm/engine/postprocessing/cloudwatch.py
@@ -32,20 +32,29 @@ def get_perf_profile_start_finish_datetime(db: DB, profile_id: int) -> Tuple[dat
 def get_perf_profile_data(db: DB, profile_id: int) -> List[Dict[str, Any]]:
     resp = db.select_with_fields(
         'SELECT id, start, finish, extra_data FROM perf_profile_entry WHERE profile_id = %s ORDER BY id',
-        params=(profile_id,)
+        params=(profile_id,),
     )
     if resp:
         return resp
 
 
-def get_cloudwatch_logs(
-        cw_client: boto3.client, log_groups: list, start_dt: datetime, finish_dt: datetime
-) -> List[List[Dict[str, str]]]:
-    """Receive Cloudwatch Logs during job execution"""
-    query = 'fields @timestamp, @message | filter @message like /REPORT RequestId/ | sort @timestamp desc | limit 10000'
+def calc_number_aws_lambda_runs(perf_profile_entries: List[Dict[str, Any]]) -> float:
+    """Calculate the number of lithops runs on AWS Lambda"""
+    runs = []
+    for entry in perf_profile_entries:
+        if entry['extra_data']['runtime_memory'] <= 10 * 1024:
+            runs.append(len(entry['extra_data']['request_ids']))
+    return sum(runs)
 
-    # extend the time range by delta seconds in both directions
-    delta = 5
+
+def get_raw_cloudwatch_logs(
+        cw_client: boto3.client, log_groups: List[str], start_dt: datetime, finish_dt: datetime
+) -> Dict[str, Any]:
+    """Return back response from Cloudwatch client"""
+
+    query = 'fields @timestamp, @message | filter @message like /REPORT RequestId/ | sort @timestamp desc | limit 10000'
+    delta = 5  # extend the time range by delta seconds in both directions
+
     start_query_response = cw_client.start_query(
         logGroupNames=log_groups,
         startTime=int((start_dt - timedelta(seconds=delta)).timestamp()),
@@ -54,16 +63,37 @@ def get_cloudwatch_logs(
     )
 
     query_id = start_query_response['queryId']
+    logger.info(f'{query_id=}')
     response = None
 
-    while response == None or response['status'] == 'Running':
-        time.sleep(1)
+    while response is None or response['status'] == 'Running':
+        time.sleep(5.0)
         response = cw_client.get_query_results(queryId=query_id)
+        logger.info(response['status'])
+        logger.info(response['statistics'])
 
+    return response
+
+
+def get_cloudwatch_logs(
+    cw_client: boto3.client, log_groups: list, start_dt: datetime, finish_dt: datetime, runs: float
+) -> List[List[Dict[str, str]]]:
+    """Return back Cloudwatch Logs during job execution"""
+
+    logger.info(f'Number of runs: {runs}')
+
+    response = get_raw_cloudwatch_logs(cw_client, log_groups, start_dt, finish_dt)
+    while int(response['statistics']['recordsMatched']) < runs:
+        logger.info(f'Inner: {int(response["statistics"]["recordsMatched"])}')
+        response = get_raw_cloudwatch_logs(cw_client, log_groups, start_dt, finish_dt)
+        time.sleep(20)
+
+    logger.info('Finished')
+    logger.info(response['statistics'])
     if int(response['statistics']['recordsMatched']) >= 10_000:
         logger.warning('Found more 10_000 matched records in Cloudwatch logs')
 
-    return response
+    return response['results']
 
 
 def extract_value(message: str) -> Dict[str, Union[float, int]]:
@@ -130,9 +160,14 @@ def calc_costs(perf_profile_entries, request_ids_stat) -> Dict[int, float]:
             total_time = (entry['finish'] - entry['start']).total_seconds()
             total_cost = calc_ec2_cost(runtime_memory, total_time)
         else:
-            time_total_aws = [request_ids_stat[r_id].get('duration_billed', 0.0) for r_id in extra_data['request_ids']]
+            time_total_aws = [
+                request_ids_stat[r_id].get('duration_billed', 0.0)
+                for r_id in extra_data['request_ids']
+            ]
             total_gb_sec = np.sum(np.array(time_total_aws) * runtime_memory / 1024)
-            total_cost = calc_lambda_cost(total_gb_sec, extra_data.get('num_actions', len(extra_data['request_ids'])))
+            total_cost = calc_lambda_cost(
+                total_gb_sec, extra_data.get('num_actions', len(extra_data['request_ids']))
+            )
 
         costs[entry['id']] = total_cost
 
@@ -143,10 +178,12 @@ def get_costs(cloudwatch_client: boto3.client, db: DB, log_groups: List[str], pr
     """Main function to calculate the cost per step for a given profile_id"""
 
     start_dt, finish_dt = get_perf_profile_start_finish_datetime(db, profile_id)
-    cloudwatch_logs = get_cloudwatch_logs(cloudwatch_client, log_groups, start_dt, finish_dt)
-    request_ids_stat = extract_data_from_cloudwatch_logs(cloudwatch_logs)
     perf_profile_entries = get_perf_profile_data(db, profile_id)
+    aws_lambda_runs = calc_number_aws_lambda_runs(perf_profile_entries)
+    cloudwatch_logs = get_cloudwatch_logs(cloudwatch_client, log_groups, start_dt, finish_dt, aws_lambda_runs)
+    request_ids_stat = extract_data_from_cloudwatch_logs(cloudwatch_logs)
     costs = calc_costs(perf_profile_entries, request_ids_stat)
+    logger.info(costs)
 
     return costs
 
