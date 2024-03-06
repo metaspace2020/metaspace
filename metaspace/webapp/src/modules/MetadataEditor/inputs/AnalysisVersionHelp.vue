5f4b7a90892f76dca5edda065a617f75a5f7ac64
Bishoy Wadie <38420248+Bisho2122@users.noreply.github.com>
2024-03-06 11:51:07 +0100

    Update AnalysisVersionHelp.vue
    Updated Analysis version help on upload page

--- metaspace/webapp/src/modules/MetadataEditor/inputs/AnalysisVersionHelp.vue
+++ metaspace/webapp/src/modules/MetadataEditor/inputs/AnalysisVersionHelp.vue
@@ -3,12 +3,25 @@
     When comparing data between multiple datasets, it is recommended to process all datasets with the same
     analysis version. Otherwise, select the latest version for best results.
     <h4>Changes:</h4>
-    <h5>v2.20230517 (METASPACE-ML):</h5>
+    <h5>Animal_v3.2023-12-14:</h5>
+    <h5>Plant_v3.2023-12-14:</h5>
     <ul>
-      <li>New metrics for evaluating annotations' mass accuracy.</li>
-      <li>New ML-powered model for annotation scoring.</li>
-      <li>Fine-grained reporting of FDR values.</li>
+      <li>New ML models trained on animal and plant datasets seperately.</li>
+      <li>ML models trained and evaluated on 1700+ datasets.</li>
+      <li>Improved annotation coverage, quality and reliability.</li>
     </ul>
+    <p>
+      Select Animal_v3.2023-12-14 or Plant_v3.2023-12-14 based on whether the submitted samples are 
+      obtained from species that are classified into animal or plant kingdom, respectively. 
+      If the samples are obtained from a different kingdom (e.g. Bacteria or Fungi), 
+      please use Animal_v3.2023-12-14 as this model is trained on a broader range of datasets 
+      and is designed to be indifferent to sample-specific metadata, focusing solely on ions and their corresponding feature scores.
+      For more information, check out our <a
+      target="_blank"
+      href="https://www.biorxiv.org/content/10.1101/2023.05.29.542736v2"
+    >bioArxiv paper</a>
+    </p>
+    
   </div>
 </template>
 
