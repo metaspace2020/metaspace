930c4b999ecb8aeed009d54917e235e2f957eeb1
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2024-10-25 15:57:59 -0700

    [graphql] created canPerformAction tests

--- metaspace/graphql/src/modules/plan/util/canPerformAction.spec.ts
+++ metaspace/graphql/src/modules/plan/util/canPerformAction.spec.ts
@@ -1,12 +1,16 @@
 import {
+  adminContext,
   onAfterAll,
   onAfterEach,
   onBeforeAll,
   onBeforeEach,
-  setupTestUsers, userContext,
+  setupTestUsers, testPlan, userContext,
 } from '../../../tests/graphqlTestEnvironment'
 
 import canPerformAction from './canPerformAction'
+import { createTestApiUsage, createTestPlanRule } from '../../../tests/testDataCreation'
+import * as moment from 'moment/moment'
+import { Moment } from 'moment'
 
 describe('modules/plan/controller (queries)', () => {
   beforeAll(onBeforeAll)
@@ -16,14 +20,68 @@ describe('modules/plan/controller (queries)', () => {
     jest.clearAllMocks()
     await onBeforeEach()
     await setupTestUsers()
+    await createTestPlanRule({
+      planId: testPlan.id,
+      actionType: 'download',
+      period: 1,
+      periodType: 'day',
+      limit: 1,
+    })
+    await createTestPlanRule({
+      planId: testPlan.id,
+      actionType: 'download',
+      period: 1,
+      periodType: 'month',
+      limit: 2,
+    })
   })
 
   afterEach(onAfterEach)
 
   describe('Plan.canPerformAction', () => {
-    it('should be able to download as admin', async() => {
-      await canPerformAction(userContext, 'download')
-      expect(1).toBe(1)
+    it('should be able to download under the limit per day', async() => {
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      await createTestApiUsage({ actionType: 'upload', userId: userContext.user.id })
+      expect(await canPerformAction(userContext, 'download')).toBe(true)
+    })
+    it('should not be able to download over the limit per day', async() => {
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      expect(await canPerformAction(userContext, 'download')).toBe(false)
+    })
+    it('should  be able to download over the limit per day if an admin', async() => {
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      expect(await canPerformAction(adminContext, 'download')).toBe(true)
+    })
+    it('should  be able to download under the limit per month', async() => {
+      await createTestApiUsage({
+        actionType: 'download',
+        userId: userContext.user.id,
+        actionDt: moment.utc().subtract(2, 'day') as unknown as Moment,
+      })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      expect(await canPerformAction(userContext, 'download')).toBe(true)
+    })
+    it('should not be able to download over the limit per month', async() => {
+      await createTestApiUsage({
+        actionType: 'download',
+        userId: userContext.user.id,
+        actionDt: moment.utc().subtract(2, 'day') as unknown as Moment,
+      })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      expect(await canPerformAction(userContext, 'download')).toBe(false)
+    })
+    it('should be able to download over the limit per month as admin', async() => {
+      await createTestApiUsage({
+        actionType: 'download',
+        userId: userContext.user.id,
+        actionDt: moment.utc().subtract(2, 'day') as unknown as Moment,
+      })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      await createTestApiUsage({ actionType: 'download', userId: userContext.user.id })
+      expect(await canPerformAction(adminContext, 'download')).toBe(true)
     })
   })
 })
--- metaspace/graphql/src/modules/plan/util/canPerformAction.ts
+++ metaspace/graphql/src/modules/plan/util/canPerformAction.ts
@@ -1,11 +1,11 @@
 import { Context } from '../../../context'
-import { Plan } from '../model'
+import { ApiUsage, Plan } from '../model'
+import * as moment from 'moment'
 
-const canPerformAction = async(ctx: Context, action: string) => {
+const canPerformAction = async(ctx: Context, actionType: string) => {
   const user: any = ctx?.user
 
   if (user?.role === 'admin') {
-    console.log(action)
     return true
   }
 
@@ -17,6 +17,24 @@ const canPerformAction = async(ctx: Context, action: string) => {
       .getOne()
   }
 
-  return false
+  const planRules = user.plan.planRules.filter((rule: any) => rule.actionType === actionType)
+
+  for (const rule of planRules as any[]) {
+    const startDate = moment.utc().startOf(rule.periodType)
+    const endDate = moment.utc().add(rule.period, rule.periodType)
+
+    const usages = await ctx.entityManager
+      .createQueryBuilder(ApiUsage, 'usage')
+      .where('usage.actionType = :actionType', { actionType: actionType })
+      .andWhere('usage.actionDt >= :startDate', { startDate: startDate.toDate() })
+      .andWhere('usage.actionDt <= :endDate', { endDate: endDate.toDate() })
+      .getMany()
+
+    if (usages.length > rule.limit) {
+      return false
+    }
+  }
+
+  return true
 }
 export default canPerformAction
