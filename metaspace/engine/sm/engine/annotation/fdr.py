2855f8278c624a5a4f35db604569645af750f498
Bishoy Wadie <bwadie298@gmail.com>
2024-11-25 19:45:10 +0100

    Exclude chem_mods from decoy selection

--- metaspace/engine/sm/engine/annotation/fdr.py
+++ metaspace/engine/sm/engine/annotation/fdr.py
@@ -136,7 +136,7 @@ class FDR:
     fdr_levels = [0.05, 0.1, 0.2, 0.5]
 
     def __init__(self, fdr_config, chem_mods, neutral_losses, target_adducts, analysis_version):
-        self.decoy_adduct_cand = [ad for ad in DECOY_ADDUCTS if ad not in target_adducts]
+        self.decoy_adduct_cand = [ad for ad in DECOY_ADDUCTS if ad not in target_adducts + chem_mods + neutral_losses]
         self.decoy_sample_size = min(fdr_config['decoy_sample_size'], len(self.decoy_adduct_cand))
 
         self.chem_mods = chem_mods
