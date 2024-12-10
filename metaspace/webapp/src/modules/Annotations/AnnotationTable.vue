a0ef267368dce3dda5937d337698b9f1c2c5ee82
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2024-12-10 13:53:57 -0800

    [DatasetAnnotation] change filters displayed on annotation access via dataset

--- metaspace/webapp/src/modules/Annotations/AnnotationTable.vue
+++ metaspace/webapp/src/modules/Annotations/AnnotationTable.vue
@@ -812,7 +812,7 @@ export default defineComponent({
       if (pageSizes.length > 0) {
         state.recordsPerPage = pageSizes[0]
       }
-      executeQuery()
+      await executeQuery()
       updateColumns()
       updateDatasetColumns()
       updateColocSort()
@@ -827,7 +827,7 @@ export default defineComponent({
       try {
         const result = await apolloClient.query({
           query: annotationListQuery,
-          variables: queryVariables.value,
+          variables: queryVariables?.value,
           fetchPolicy: 'cache-first',
           throttle: 200,
         })
@@ -862,14 +862,14 @@ export default defineComponent({
           state.nextCurrentRowIndex = null
         } else if (state.nextCurrentRowIndex !== -1) {
           const curRow = getCurrentRow()
-          if (!curRow.value) {
-            setCurrentRow(currentRowIndex.value)
+          if (!curRow?.value) {
+            setCurrentRow(currentRowIndex?.value)
           }
         }
         // Move focus to the table so that keyboard navigation works, except when focus is on an input element
         const shouldMoveFocus = document.activeElement?.closest('input,select,textarea') == null
-        if (table.value && shouldMoveFocus) {
-          table.value?.$el.focus()
+        if (table?.value && shouldMoveFocus) {
+          table?.value?.$el.focus()
         }
 
         // load ROIs from db
@@ -884,7 +884,7 @@ export default defineComponent({
     }
 
     const hidden = (columnLabel) => {
-      return state.columns.findIndex((col) => col.src === columnLabel) === -1 || !showCustomCols.value
+      return state.columns.findIndex((col) => col.src === columnLabel) === -1 || !showCustomCols?.value
         ? props.hideColumns.indexOf(columnLabel) >= 0 || !state.columns.find((col) => col.src === columnLabel)?.selected
         : !state.columns.find((col) => col.src === columnLabel)?.selected
     }
