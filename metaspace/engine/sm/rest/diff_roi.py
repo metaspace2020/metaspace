0f58ebde1ae7123ccb2a5d021b8bd111317bd9ee
Bishoy Wadie <bwadie298@gmail.com>
2026-02-03 21:17:10 +0100

    Diff ROI manager methods to classes

--- metaspace/engine/sm/rest/diff_roi.py
+++ metaspace/engine/sm/rest/diff_roi.py
@@ -7,7 +7,7 @@ import json
 import bottle
 
 from sm.rest.utils import body_to_json, make_response, OK, INTERNAL_ERROR
-from sm.rest.diff_roi_manager import run_diff_roi, save_diff_roi_results
+from sm.rest.diff_roi_manager import DiffROIAnalysis
 
 logger = logging.getLogger('api')
 app = bottle.Bottle()
@@ -19,14 +19,17 @@ def compare_rois():
         ds_id = params['ds_id']
 
         start = time.time()
-        comp_roi_result = run_diff_roi(ds_id= ds_id,
-                                       TIC_normalize=params.get('TIC_normalize', True),
-                                       log_transform_tic=params.get('log_transform_tic', True),
-                                       chunk_size=params.get('chunk_size', 100),
-                                       n_pixel_samples=params.get('n_pixel_samples', 10000))
+        analysis = DiffROIAnalysis(
+            ds_id=ds_id,
+            TIC_normalize=params.get('TIC_normalize', True),
+            log_transform_tic=params.get('log_transform_tic', True),
+            chunk_size=params.get('chunk_size', 100),
+            n_pixel_samples=params.get('n_pixel_samples', 10000),
+        )
+        comp_roi_result = analysis.run_diff_roi()
         logger.info(f'Completed diff ROI in {round(time.time() - start, 2)} sec')
 
-        save_diff_roi_results(ds_id=ds_id, comp_roi_df=comp_roi_result)
+        analysis.data.save_diff_roi_results(comp_roi_result)
         logger.info(f'Saved diff ROI results for dataset {ds_id}')
         # body = {'diff_roi_result': comp_roi_result.to_dict(orient='records')}
         return make_response(OK)
